429c80b2c072bda7fe66396b73c051c7397185f5
Nildo <nildo@webstack.com.br>
2021-09-15 07:58:24 -0300

    Adicioando arquivo console.log ao projeto

---
+++ introducao/consoleLog.js
@@ -0,0 +1,18 @@
+/**
+ * DEFINIÇÃO E USO
+ * O método console.log(), é uma função do JavaScript, para exibir qualquer
+ * tipo de conteúdo em sua saida padrão, sejam elas mensagens de textos, 
+ * numeros entre outros valores. 
+ */
+
+// Imprimindo uma cadaia de caracteres diretamente no console
+console.log('Eu sou um texto.');
+
+// Exibindo variável do tipo string, como parâmetro no console
+let cor = 'amarelo';
+console.log(cor); 
+
+// Exibindo variável do tipo inteiro, como parâmetro no console
+let ano = 2000; 
+console.log(ano);
+
