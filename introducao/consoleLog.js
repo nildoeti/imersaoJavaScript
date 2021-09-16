28ff0f9d619bf0a2b8cf6199b7f1aed69473a5f0
Nildo <nildo@webstack.com.br>
2021-09-16 18:30:48 -0300

    Atualizado arquivo consoleLog.js

--- introducao/consoleLog.js
+++ introducao/consoleLog.js
@@ -1,18 +1,20 @@
 /**
  * DEFINIÇÃO E USO
  * O método console.log(), é uma função do JavaScript, para exibir qualquer
- * tipo de conteúdo em sua saida padrão, sejam elas mensagens de textos, 
- * numeros entre outros valores. 
+ * tipo de conteúdo em sua saida padrão, sejam elas mensagens de textos,
+ * numeros entre outros valores.
  */
 
-// Imprimindo uma cadaia de caracteres diretamente no console
+// Imprimindo uma cadeia de caracteres diretamente no console
 console.log('Eu sou um texto.');
 
 // Exibindo variável do tipo string, como parâmetro no console
 let cor = 'amarelo';
-console.log(cor); 
+console.log(cor);
 
 // Exibindo variável do tipo inteiro, como parâmetro no console
-let ano = 2000; 
+let ano = 2000;
 console.log(ano);
 
+// exibindo vários valores diferentes no console com uso da vírgula
+console.log(35, 3.45, 'cadeia de caracteres')
