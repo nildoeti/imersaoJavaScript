4bb83214491d5329a796008275e2da3247bc7f05
Nildo <nildo@webstack.com.br>
2021-09-22 17:22:21 -0300

    operadoresAtribuicaoIncremento.js atualizado.

--- introducao/operadoresAtribuicaoIncremento.js
+++ introducao/operadoresAtribuicaoIncremento.js
@@ -44,24 +44,50 @@ console.log('(10 + 2) x 2 = ', (x + y) * y);
 console.log('(10 x 2) + 2 = ', (x * y) + y);
 
 // INCREMENTO
-let velocidade1 = 0; // variável inicializada com valor 1
-// incrementa velocidade1 em 1 e retorna o antigo valor
-velocidade1++;
-console.log(velocidade1); // 0
+let velocidade = 0; // variável inicializada com valor 1
 
-let velocidade2 = 0; // variável inicializada com valor 1
-// incrementa velocidade em 2 m 1, e retorna o novo valor
-++velocidade2;
-console.log(velocidade2); // 1
+velocidade++; // incrementa velocidade1 em 1 e retorna o antigo valor
+console.log('velocidade++: ', velocidade); // 0
 
+++velocidade; // incrementa velocidade em 2 m 1, e retorna o novo valor
+console.log('++velocidade: ', velocidade); // 2
 
 // DECREMENTO
-let contador1 = 0;
-// decrementa velocidade em 1 e retorna o antigo valor
-contador1--;
-console.log(contador1)
 
-// decrementa velocidade em 1 e retorna o novo valor
-let contador2 = 0;
---contador2;
-console.log(contador2)
+velocidade--; // pós decrementa velocidade em 1 e retorna o antigo valor
+console.log('velocidade--: ', velocidade); // 1
+
+--velocidade; // pré decrementa velocidade em 1, e retorna o novo valor
+console.log('--decremento: ', velocidade); // 0
+
+
+// INCREMENTO com mais de um valor
+// const passo = 2; // para cada dois passos
+// let distancia = 0
+//
+// distancia = distancia + passo;
+// console.log('\nPara cada dois passos que ando, minha distância é: ', distancia);
+// distancia = distancia + passo;
+// console.log('Para cada dois passos que ando, minha distância é: ', distancia);
+// distancia = distancia + passo;
+// console.log('Para cada dois passos que ando, minha distância é: ', distancia);
+
+// Outra forma de fazer o mesmo exemplo acima
+// let contador = 0;
+// contador+= 2;
+// contador+= 2;
+// contador+= 2;
+// console.log('\n', contador);
+
+// Outra forma de fazer o mesmo exemplo acima só que o operador de multiplicação
+// let contador = 1;
+// contador *= 2;
+// contador *= 2;
+// contador *= 2;
+// contador *= 2;
+// console.log('\n', contador);
+
+// O mesmo do exemplo acima só que com o operador de potênciação
+let contador = 2;
+contador**= 3;
+console.log('\n2³ = ', contador);
