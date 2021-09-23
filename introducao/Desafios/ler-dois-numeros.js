cd9433e26a29f42e59d0bb6e4f6dd45ac03abb3f
Nildo <nildo@webstack.com.br>
2021-09-22 21:01:22 -0300

    ler-dois-numeros.js atualizado.

---
+++ introducao/Desafios/ler-dois-numeros.js
@@ -0,0 +1,6 @@
+let primeiroNumero = Number(prompt('Digite o primero número: '));
+let segundoNumero = Number(prompt('Digite o segundo número: '));
+
+let resultado = primeiroNumero + segundoNumero;
+
+alert(`O resultado da soma é ${resultado}`);
