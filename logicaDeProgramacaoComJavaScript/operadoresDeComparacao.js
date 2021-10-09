9491b7bd90e4acec0ffa1904e96342812bf0a881
Nildo <nildo@webstack.com.br>
2021-10-09 08:28:16 -0300

    operadoresDeComparacao.js atualizado.

--- logicaDeProgramacaoComJavaScript/operadoresDeComparacao.js
+++ logicaDeProgramacaoComJavaScript/operadoresDeComparacao.js
@@ -10,6 +10,7 @@
     !== diferente estrito de valor e tipo
 */
 
+// Primeiro exemplo com uso de números inteiros
 console.log(10 > 5); // true
 console.log(10 >= 5); // true
 console.log(10 < 5); // false
@@ -18,3 +19,50 @@ console.log(10 == 5); // false
 console.log(10 === 5); // false
 console.log(10 != 5); // true
 console.log(10 !== 5); // true
+
+// Segundo exemplo com uso de varáveis do tipo numéricas
+let maior = 10;
+let menor = 5;
+
+console.log(maior > menor); // true
+console.log(maior >= menor); // true
+console.log(maior < menor); // false
+console.log(maior <= menor); // false
+console.log(maior == menor); // false
+console.log(maior === menor); // false
+console.log(maior != menor); // true
+console.log(maior !== menor); // true
+
+/*
+    Terceiro exemplo de variáveis do tipo strings.
+
+    IMPORTANTE
+    Em casos do uso de operadors de comparação, o JavaScript usa a posição
+    de cada caracteres para fazer a comparação.
+
+    Para efeito de comparação, podemo usar as vogais como exemplo.
+    'a' é menor que 'e', que é menor que 'i', que é menor que 'o', que é menor
+    que 'u'.
+
+    Nos casos das consoantes também é valido a mesma ordem conforme exemplo.
+    'b' é menor que 'c', que é menor que 'd', que é menor que 'f'....
+
+    E assim também é valido a mesma declaração para valores do tipo numerál.
+    1 é menor que 2, que é menor que 3, que é menor que 4 .....
+
+    Sendo assim, a posição e não o comprimento de uma cadeira de caracteres,
+    que influência na forma como os operadores de comparação retorna um valor
+    verdadeiro ou falso.
+*/
+
+let vogalA = 'a'; // comprimento da string = 5
+let vogalE = 'e'; // comprimento da string = 9
+
+console.log(vogalA > vogalE); // falso
+console.log(vogalA >= vogalE); // falso
+console.log(vogalA < vogalE); // true
+console.log(vogalA <= vogalE); // true
+console.log(vogalA == vogalE); // false
+console.log(vogalA === vogalE); // false
+console.log(vogalA != vogalE); // true
+console.log(vogalA !== vogalE); // true
