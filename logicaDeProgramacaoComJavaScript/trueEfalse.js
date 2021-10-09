0c552b17d853ac1f307c03239d075f8adc6614a1
Nildo <nildo@webstack.com.br>
2021-10-09 14:23:37 -0300

    trueEfalse.js foi atualizado

---
+++ logicaDeProgramacaoComJavaScript/trueEfalse.js
@@ -0,0 +1,15 @@
+/**
+ * 	Operação aritimética com true e false em JavaScript
+ */
+
+console.log(true + 0); // 1 + 0 = 1
+console.log(true + true); // 1 + 1 = 2
+console.log(true - true); // 1 - 1 = 0
+console.log(true - false); // 1 - 0: 1
+console.log(false + false); // 0 + 0 = 0
+console.log(false - false); // 0 - 0 = 0
+console.log(true * true); // 1 * 1 = 1
+console.log(true * false); // 1 * 0 = 0
+console.log(false * false); // 0 * 0 = 0
+console.log(true / true); // 1 / 1 = 1
+console.log(true / false); // Em JavaScript, qualquer número dividido por 0 é (Infinity)
