8aba028ee3c911cebc5dbd591e61259be8064d6d
Nildo <nildo@webstack.com.br>
2021-10-08 22:43:40 -0300

    operadoresDeComparacao.js atualizado.

---
+++ logicaDeProgramacaoComJavaScript/operadoresDeComparacao.js
@@ -0,0 +1,20 @@
+/*
+    OPERADORES DE COMPARAÇÃO
+    > maior que
+    >= maior que ou igual
+    < menor que
+    <= menor que ou igual
+    == igualdade de valor
+    === igualdade estrita de valor e tipo
+    != diferente de valor
+    !== diferente estrito de valor e tipo
+*/
+
+console.log(10 > 5); // true
+console.log(10 >= 5); // true
+console.log(10 < 5); // false
+console.log(10 <= 5); // false
+console.log(10 == 5); // false
+console.log(10 === 5); // false
+console.log(10 != 5); // true
+console.log(10 !== 5); // true
