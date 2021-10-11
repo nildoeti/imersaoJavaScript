c413c1c9197c5e2dca18771acd867f3b4d173435
Nildo <nildo@webstack.com.br>
2021-10-11 18:45:32 -0400

    trueEfalse.js foi atualizado.

--- logicaDeProgramacaoComJavaScript/trueEfalse.js
+++ logicaDeProgramacaoComJavaScript/trueEfalse.js
@@ -13,3 +13,4 @@ console.log(true * false); // 1 * 0 = 0
 console.log(false * false); // 0 * 0 = 0
 console.log(true / true); // 1 / 1 = 1
 console.log(true / false); // Em JavaScript, qualquer número dividido por 0 é (Infinity)
+
