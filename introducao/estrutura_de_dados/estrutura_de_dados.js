9446a0c993a2cd857be832a07d2a9d2a3adc4987
Nildo <76877302+nildoeti@users.noreply.github.com>
2021-11-02 17:36:25 -0300

    estrutura_de_dados.js foi atualizado.

---
+++ introducao/estrutura_de_dados/estrutura_de_dados.js
@@ -0,0 +1,26 @@
+/**
+    ESTRUTURAS DE DADOS EM JAVASCRIPT
+
+    JavaScript é:
+        linguagem de tipagem dinãmica, o que a torna diferente de liguagens
+        como C, Java, que exige a declaração do tipo de variável.
+
+
+    TIPOS DE DADOS EM JAVASCRIPT
+
+    TIPOS PRIMITIVOS
+    numbers, strings, booleans, null, undefined
+
+    TIPOS COMPOSTOS / NÃO PRIMITIVOS
+    objects, arrays
+*/
+
+
+let numero = 1;
+console.log(typeof(numero)); // tipo number
+
+let cor = 'azul';
+console.log(typeof(cor)); // tipo string
+
+let logico = true;
+console.log(typeof(logico)); // tipo boolean
