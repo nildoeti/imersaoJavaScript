9d1146cc8061bb6fdf691dc7002c0a1cbf636afd
Nildo <76877302+nildoeti@users.noreply.github.com>
2021-11-02 20:39:56 -0300

    introducao/booleans/booleans.js foi atualizado.

---
+++ introducao/booleans/booleans.js
@@ -0,0 +1,13 @@
+/**
+ * Valores boobleans retornan sempre valors do 
+ * tipo true (verdade) ou false (falso)
+ */
+
+let valorUm = 4;
+let valorDois = 2;
+
+let primeiraComparacao  = valorUm == valorDois;
+console.log(primeiraComparacao); // false
+
+let segundaComparacao = valorUm > valorDois;
+console.log(segundaComparacao); // true
