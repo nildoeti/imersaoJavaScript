71c35ecab18719640d812a15bc53e21069ea49e5
Nildo <76877302+nildoeti@users.noreply.github.com>
2021-11-02 16:58:56 -0300

    var_let_const.js foi atualizado.

---
+++ introducao/var_let_const/var_let_const.js
@@ -0,0 +1,9 @@
+/**
+    var, let, const
+
+                        VAR             CONST   LET
+    escopo              global/local    bloco   bloco
+    redeclarar          SIM             NÃO     NÃO
+    reatribuir          SIM             NÃO     SIM
+    hoisting            SIM             NÃO     NÃO
+*/
