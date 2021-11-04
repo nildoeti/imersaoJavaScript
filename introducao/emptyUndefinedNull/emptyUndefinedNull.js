6ac8f7768623c7fe85b8edd512c5c97886849d76
Nildo <nildo@webstack.com.br>
2021-11-03 22:48:52 -0300

    introducao/emptyUndefinedNull/emptyUndefinedNull.js foi atualizado.

---
+++ introducao/emptyUndefinedNull/emptyUndefinedNull.js
@@ -0,0 +1,14 @@
+/**
+ * empyt
+ * Uma declaração vazia é usada para fornecer nenhuma declaração, embora a 
+ * sintaxe do JavaScript esperasse uma.
+ * 
+ * undefined
+ * O valor global undefined representa um valor indefinido. 
+ * Uma variável que não teve um valor atribuído é do tipo undefined. 
+ */
+
+// undefine
+let nome; // uma variável com valor indefinido
+console.log(nome); 
+
