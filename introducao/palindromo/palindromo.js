40f40f2d737ea0f084e30a140215cd61cc474eeb
Nildo <nildo@webstack.com.br>
2021-11-04 18:07:05 -0300

    palindromo.js foi atualizado.

--- introducao/palindromo/palindromo.js
+++ introducao/palindromo/palindromo.js
@@ -1,22 +1,23 @@
 // solução 1
-function verificaPalindromo(string) {
-	if (!string) return;
-	console.log(string === string.split('').reverse().join(''));
+
+function verificaPolindromo(string) {
+    if(!string) return;
+    console.log(string === string.split('').reverse().join());
 }
-verificaPalindromo('cat');
+
+verificaPolindromo('maria'); // se existir string aqui, retornar false
 
 // solução 2
+function verificaPolindromo2(string) {
+    if (!string) return;
+    if (!string.length) return;
 
-function varificaPalindromo2(string) {
-	if (!string) return;
-	if (!string.length) return;
-
-	for (var i = 0; i < string.length / 2; i++) {
-		if (string[i] !== string[string.length - 1 - i]) {
-			return console.log(false);
-		}
-	}
-	return console.log(true);
+    for (var i = 0; i < string.length / 2; i++) {
+        if (string[i] !== string[string.length - 1 - i]) {
+            console.log(false);
+        }
+    }
+    return console.log(true);
 }
 
-varificaPalindromo2('asa');
+verificaPolindromo2('teste');
