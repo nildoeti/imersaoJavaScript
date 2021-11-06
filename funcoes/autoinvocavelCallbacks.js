0ed503078993e46a9f992339db6485a47c5061ba
Nildo <nildo@webstack.com.br>
2021-11-06 09:09:42 -0300

    funcoes/parametros.js foi adicionado ao projeto.

---
+++ funcoes/autoinvocavelCallbacks.js
@@ -0,0 +1,52 @@
+// FUNCÕES AUTOINVOCÁVEL
+// é uma função anônima entre parênteses seguida de um par de parênteses que 
+// representa sua chamada
+
+(
+    function() {
+        let name = 'Qualquer nome'
+        return name;
+    } 
+)();
+
+
+// FUNÇÕS AUTOINVOCÁVEL COM USO PARÂMETROS
+
+(
+    function(a, b) { // parãmetros: a, b
+        return a + b;
+    }
+) (1, 2); // 3
+
+// FUNÇÕS AUTOINVOCÁVEL COM USO PARÂMETROS E ARMAZENADAS EM UMA VARIÁVEL
+const soma3 = (
+    function() {
+        return a + b;
+    }
+)(2, 3);
+
+
+// CALBACKS
+// funcões passadas como argumentos para outras funções, sendo uma forma
+// de melhor controle de ordem de chamadas
+
+// exemplo 1
+const calc = function(operacao, num1, num2) {
+    return operacao(num1, num2);
+}
+
+// exemplo 2
+const soma = function(num1, num2) {
+    return num1 + num2;
+}
+
+// exemplo 3
+const sub = function(num1, num2) {
+    return num1 - num2;
+}
+
+const resultadoSoma = cal(soma, 1, 2);
+const resultadoSub = calc(sub, 1, 2);
+
+console.log(resultadoSoma);
+console.log(resultadoSub);
