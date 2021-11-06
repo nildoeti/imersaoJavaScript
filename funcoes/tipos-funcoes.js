21a66828f6b5c92ddddb15bca51677f8d01ff152
Nildo <nildo@webstack.com.br>
2021-11-06 08:40:06 -0300

    funcoes/tipos-funcoes.js foi atualizado

---
+++ funcoes/tipos-funcoes.js
@@ -0,0 +1,30 @@
+/**
+ * DEFINIÇÃO DE UMA FUNÇÃO 
+ * 
+ * function nome(parametros) {
+ *  // instruções a ser executadas
+ * }
+ * 
+ * VARIAVEIS
+ * se dentro da função, somente excutadas nela, não fora
+ * 
+ * RETURN
+ * para a execução da função
+ * 
+ * function nome(parametros) {
+ *  // instruções a ser executadas
+ *  return; // valor de retorno da função
+ * }
+ * 
+ * FUNÇÕES ANÔNIMA
+ * funções que não tem nome e representam expressões em que uma variável 
+ * pode armazenar uma variável
+ * 
+ * const soma = function (a, b) {
+ *  return a + b;
+ * }
+ * soma(1, 2) // 3
+ * soma(5, 4) // 9
+ * 
+ * 
+ */
