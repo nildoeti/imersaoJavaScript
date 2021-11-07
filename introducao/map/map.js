565828ebc8a023b431f1369cd8ae3300b9316169
Nildo <nildo@webstack.com.br>
2021-11-07 10:35:09 -0300

    has been added to the project

---
+++ introducao/map/map.js
@@ -0,0 +1,14 @@
+// MAP
+
+
+// declaration
+const myMap = new Map()
+console.log(myMap); // Map(0) {}
+
+
+// DEFINITIONS
+let resultOne = myMap.set('apple', 'fruit');
+console.log(resultOne); // Map(1) { 'apple' => 'fruit' }
+
+let resultTwo = myMap.get('aplle');
+console.log(resultTwo);
