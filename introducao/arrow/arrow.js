ab3c2505888ea9078195ca74eeec99fa452ba64e
Nildo <nildo@webstack.com.br>
2021-11-07 00:20:56 -0300

    arrow.js

---
+++ introducao/arrow/arrow.js
@@ -0,0 +1,50 @@
+// ARROW FUNCTION: =>
+
+// example 1
+const helloWord = function() {
+	return 'Hello World';
+}
+
+// example 2
+const helloWord = () => { // arrow function
+	return 'Hello World';
+}
+
+
+// example 3
+const helloWord = () => 'Hello World';
+
+
+// example 4
+
+const soma(a, b) {
+	return a + b;
+} 
+
+soma(2, 3)
+
+
+// example 5
+const sum(a, b) => a + b;
+soma(2, 3);
+
+
+// example 6
+const soma = a => b;
+soma(4);
+
+
+
+// Arrow functiom it doesn't work 
+sum(3, 5);
+const sum = (a, b) => a + b;
+
+// Ok
+sum(2, 4);
+
+function sum(a, b) {
+	return = a + b;
+}
+
+
+  
