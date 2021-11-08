7b9fdaff791c69e2ad761b5229f4b3fa29e1d096
Nildo <nildo@webstack.com.br>
2021-11-08 17:36:37 -0300

    script/main.mjs foi atualizado

---
+++ introducao/Desafios/modulos/atividades/script/main.mjs
@@ -0,0 +1,9 @@
+import {
+	mostraIdade,
+	mostraCidade,
+	mostraHobby
+} from '../funcoes.mjs';
+
+console.log(mostraIdade(`Beltrano`, 20));
+console.log(mostraCidade(`Ciclano`, 'São Paulo'));
+console.log(mostraHobby('Fulano', 'ler'));
