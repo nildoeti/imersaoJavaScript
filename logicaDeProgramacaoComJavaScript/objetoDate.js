09be7620af1f24d08f3c7bf42d03b4d9cb0ae0f8
Nildo <nildo@webstack.com.br>
2021-11-11 11:04:17 -0300

    Gerado novas atualizações nos arquivos do projeto

---
+++ logicaDeProgramacaoComJavaScript/objetoDate.js
@@ -0,0 +1,54 @@
+// const tresHoras = 60 * 60 * 3 * 1000;
+// const umDia = 60 * 60 * 24 * 1000;
+
+// const data = new Date(0 + tresHoras + umDia);
+// console.log(data.toString()); 
+
+
+// const data = new Date(2021, 03, 20, 15, 30, 45); // aaaa, mm, dd, hh, mm, ss
+
+// const data = new Date('2021-04-10T20:40:21.100');
+// console.log(data.toString());
+
+// const data = new Date();
+
+// console.log(`Dia: ${data.getDate()}`);
+// console.log(`Mês: ${data.getMonth()}`);
+// console.log(`Ano: ${data.getFullYear()}`);
+// console.log(`Hora: ${data.getHours()}`);
+// console.log(`Minuto: ${data.getMinutes()}`);
+// console.log(`Segundos: ${data.getSeconds()}`);
+// console.log(`Milesegundos: ${data.getMilliseconds()}`);
+// console.log(`Dia da semana: ${data.getDay()}`);
+// console.log(`Data completa: ${data.toString()}`);
+
+// const myDate = new Date();
+
+// console.log(`Date: ${myDate.getDate()}`);
+// console.log(`Month: ${myDate.getMonth() + 1}`);
+// console.log(`Year: ${myDate.getFullYear()}`);
+// console.log(`Hour: ${myDate.getHours()}`);
+// console.log(`Minutes: ${myDate.getMinutes()}`);
+// console.log(`Seconds: ${myDate.getSeconds()}`);
+// console.log(`Milliseconds: ${myDate.getMilliseconds()}`);
+// console.log(`Day of the week: ${myDate.getDay()}`);
+// console.log(`Full date: ${myDate.toString()}`);
+
+// console.log(`Current date: ${Date.now()}`); // milliseconds from the current date
+
+// const myNewDate = new Date(1636541835813); // using milliseconds
+// console.log(myNewDate); // current date
+
+
+// function formatDate(myDate) {
+// 	console.log(myDate);
+// }
+
+// const myDate = new Date();
+// formatDate(myDate);
+
+function formatDate(date) {
+	// body...
+	console.log(date);
+}
+
