137ad4beeb98541ef48721d78a6b25e3a7905af2
Nildo <nildo@webstack.com.br>
2021-12-01 19:02:03 -0300

    findingHTMLelementsByTagName-2.js foi atualizado.

---
+++ DOM/js/findingHTMLelementsByTagName-2.js
@@ -0,0 +1,5 @@
+const x = document.getElementById('main');
+const y = x.getElementsByTagName('p');
+
+document.getElementById('demo').innerHTML =
+'The first paragraph (index 0) inside "main" is: ' + y[0].innerHTML;
