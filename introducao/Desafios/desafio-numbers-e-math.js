72e27e96b891bfbf33bcb0e358ea0b8f2191a94f
Nildo <nildo@webstack.com.br>
2021-12-03 09:27:50 -0300

    desafio-numbers-e-math.js foi atualizado.

--- introducao/Desafios/desafio-numbers-e-math.js
+++ introducao/Desafios/desafio-numbers-e-math.js
@@ -1,28 +1,43 @@
-const numero = Number(prompt('Digite o primeiro número: '));
+// Exemplo 2
+const numero = Number(prompt('Digite um número: '));
 const numeroTitulo = document.getElementById('numero-titulo');
+const texto = document.getElementById('texto');
+
 numeroTitulo.innerHTML = numero;
+texto.innerHTML += `<p>${numero}² = ${numero * numero}</p>`;
+texto.innerHTML += `<p>${numero} é inteiro? = ${Number.isInteger(numero)}</p>`;
+texto.innerHTML += `<p>${numero} não é NaN? = ${Number.isNaN(numero)}</p>`;
+texto.innerHTML += `${numero} arredondado para cima é ${Math.ceil(numero)}`;
+texto.innerHTML += `<p>${numero} arredondado para baixo é ${Math.floor(numero)}</p>`;
+texto.innerHTML += `<p>${numero} com duas casas decimais é ${numero.toFixed(2)}</p>`;
 
-
-// numero quadrado
-const numeroQuadrado = document.getElementById('quadrado');
-numeroQuadrado.innerHTML = `${numero}² : ${numero * numero}`;
-
-// é inteiro?
-const numeroInteiro = document.getElementById('inteiro');
-numeroInteiro.innerHTML = `${numero} é inteiro? ${Number.isInteger(numero)}`;
-
-// NaN
-const naoNumero = document.getElementById('naoNum');
-naoNumero.innerHTML = `${numero} não é um número? ${Number.isNaN(numero)}`;
-
-// arredonda para cima
-const arredondaCima = document.getElementById('arredondaC');
-arredondaCima.innerHTML = `${numero} arredondado para cima é ${Math.ceil(numero)}`;
-
-// arredonda para baixo
-const arredondaParaBaixo = document.getElementById('arredondaB');
-arredondaParaBaixo.innerHTML = `${numero} arredondado para baixo é ${Math.floor(numero)}`;
-
-// fixa em duas casa decimal
-const fixaEmDuasCasaDecimal = document.getElementById('casaDecimal');
-fixaEmDuasCasaDecimal.innerHTML = `${numero} fixado em duas casas decimais é ${numero.toFixed(2)}`;
+// Exemplo 1
+// const numero = Number(prompt('Digite o primeiro número: '));
+//
+// const numeroTitulo = document.getElementById('numero-titulo');
+// numeroTitulo.innerHTML = numero;
+//
+//
+// // numero quadrado
+// const numeroQuadrado = document.getElementById('quadrado');
+// numeroQuadrado.innerHTML = `${numero}² : ${numero * numero}`;
+//
+// // é inteiro?
+// const numeroInteiro = document.getElementById('inteiro');
+// numeroInteiro.innerHTML = `${numero} é inteiro? ${Number.isInteger(numero)}`;
+//
+// // NaN
+// const naoNumero = document.getElementById('naoNum');
+// naoNumero.innerHTML = `${numero} não é um número? ${Number.isNaN(numero)}`;
+//
+// // arredonda para cima
+// const arredondaCima = document.getElementById('arredondaC');
+// arredondaCima.innerHTML = `${numero} arredondado para cima é ${Math.ceil(numero)}`;
+//
+// // arredonda para baixo
+// const arredondaParaBaixo = document.getElementById('arredondaB');
+// arredondaParaBaixo.innerHTML = `${numero} arredondado para baixo é ${Math.floor(numero)}`;
+//
+// // fixa em duas casa decimal
+// const fixaEmDuasCasaDecimal = document.getElementById('casaDecimal');
+// fixaEmDuasCasaDecimal.innerHTML = `${numero} fixado em duas casas decimais é ${numero.toFixed(2)}`;
