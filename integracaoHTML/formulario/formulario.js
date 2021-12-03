1a6dea5531bcc45b4344565acdb84aa0b3368d25
Nildo <nildo@webstack.com.br>
2021-12-03 10:11:57 -0300

    integracaoHTML/formulario/formulario.js deletado do projeto.

--- integracaoHTML/formulario/formulario.js
+++
@@ -1,7 +0,0 @@
-function mostrarOla() {
-	var nome = document.getElementById("nome").value;
-	document.getElementById("reposta").textContent = "Olá " + nome;	
-}
-
-var mostrar = document.getElementById("mostrar");
-mostrar.addEventListener("click", mostrarOla);
