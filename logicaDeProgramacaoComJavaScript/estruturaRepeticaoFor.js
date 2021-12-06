c3d3e76786ef5f479a7711a259f997b80b1d02ca
Nildo <nildo@webstack.com.br>
2021-12-06 12:47:54 -0300

    estruturaRepeticaoFor.js foi atualizado.

---
+++ logicaDeProgramacaoComJavaScript/estruturaRepeticaoFor.js
@@ -0,0 +1,53 @@
+// percorrer uma lista com o laço for
+const listaFrutas = [
+    'pera', // Índice 0
+    'maça', // Índice 1
+    'mamão', // Índice 2
+    'morango' // Índice 3
+    ];
+for (let i = 0; i < listaFrutas.length; i ++) {
+    console.log(`Índice ${i}: `, listaFrutas[i]);
+}
+
+
+// check se um número é par 2
+// for (let i = 0; i <= 10; i++) {
+//     const par = i % 2 === 0? 'par' : 'impar';
+//     console.log(`${i} é ${par}`);
+// }
+
+// check se um número é par 1
+// for (let i = 0; i <= 10; i++) {
+//     const par = i % 2 === 0;
+//     console.log(`${i}: ${par}`);
+// }
+
+// for clássico 5
+// for (let i = 500; i > 400; i -= 10) {
+//     console.log(i);
+// }
+
+// for clássico 4
+// for (let i = -100; i < 200; i += 10) {
+//     console.log(i);
+// }
+
+// for clássico 3
+// for (let i = 500; i <= 600; i += 10) {
+//     console.log(i);
+// }
+
+
+// for clássico 2
+// for (let i = 100; i < 200; i++) {
+//     console.log(i);
+// }
+
+
+// for clássico 1
+// for (let i = 0; i < 5; i++) {
+//     // let i (index), é nossa variável de controle
+//     // nosso laço for, so vai parar quando a variável i, for menor qye 5
+//     // i++ como sendo incremento para a condição de i.
+//     console.log(i);
+// }
