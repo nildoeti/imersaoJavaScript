deb98c66cc959a50117a2eecb9db28861d58279a
Nildo <nildo@webstack.com.br>
2021-12-08 10:47:14 -0300

    forIn.js foi atualizado com array

---
+++ logicaDeProgramacaoComJavaScript/forIn.js
@@ -0,0 +1,19 @@
+// exemplos do for in com array
+const frutas = ['pera', 'banana', 'maça', 'caqui'];
+
+// lendo os índices de um array
+// for (let i in frutas) {
+//     console.log(i);
+// }
+
+// exibindo os valores de uma array
+
+for (let indice in frutas) {
+    console.log(frutas[indice]);
+}
+
+
+// forma clássica do laço for
+// for (let i = 0; i < frutas.length; i++) {
+//     console.log(frutas[i]);
+// }
