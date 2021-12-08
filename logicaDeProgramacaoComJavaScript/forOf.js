a9c6ef92c258143578783533acfb23a172d64c15
Nildo <nildo@webstack.com.br>
2021-12-08 11:17:18 -0300

    Adicioando Exemplos do for in com strings

---
+++ logicaDeProgramacaoComJavaScript/forOf.js
@@ -0,0 +1,17 @@
+const cor = 'verde água';
+
+// exemplo 3 com for of
+for (let letras of cor) {
+    console.log(letras);
+}
+
+// exemplo 2
+// for (let letras in cor) {
+//     console.log(cor[letras]);
+// }
+
+
+// exemplo 1
+// for (let letras = 0; letras < cor.length; letras++) {
+//     console.log(cor[letras]);
+// }
