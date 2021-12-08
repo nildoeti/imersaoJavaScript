c8518534815672d6c3a168c37477a3c7ea31672b
Nildo <nildo@webstack.com.br>
2021-12-08 11:00:18 -0300

    Adicionado exemplo de for in com objetos em JavaSCript.

--- logicaDeProgramacaoComJavaScript/forIn.js
+++ logicaDeProgramacaoComJavaScript/forIn.js
@@ -1,5 +1,40 @@
+
+// exemplos do for in com object
+const pessoa = {
+    nome: 'Fulano',
+    sobreNome: 'de Tal',
+    idade: 500,
+    pais: 'Brasil'
+};
+
+// duas formas de acessar as propriedades de um object
+// console.log(pessoa.nome);
+// console.log(pessoa['nome']);
+
+// acessando propriedades do object com uso de uma variável
+// const chave = 'nome';
+// console.log(pessoa[chave]);
+
+// acessando chaves do object com for in
+// for (let chave in pessoa) {
+//         console.log(chave);
+// }
+
+// acessando propriedades da chave do object
+// for (let propriedade in pessoa) {
+//     console.log(pessoa[propriedade]);
+// }
+
+// acessando propriedades e valores de um object
+for (let propriedade in pessoa) {
+    console.log(propriedade + ': '+  pessoa[propriedade]);
+}
+
+
+
+
 // exemplos do for in com array
-const frutas = ['pera', 'banana', 'maça', 'caqui'];
+// const frutas = ['pera', 'banana', 'maça', 'caqui'];
 
 // lendo os índices de um array
 // for (let i in frutas) {
@@ -7,10 +42,10 @@ const frutas = ['pera', 'banana', 'maça', 'caqui'];
 // }
 
 // exibindo os valores de uma array
-
-for (let indice in frutas) {
-    console.log(frutas[indice]);
-}
+//
+// for (let indice in frutas) {
+//     console.log(frutas[indice]);
+// }
 
 
 // forma clássica do laço for
