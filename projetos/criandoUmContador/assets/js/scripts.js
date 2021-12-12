a775b728ed91778cce27dea9d2dd395a43be8b6f
Nildo <nildo@webstack.com.br>
2021-12-12 08:15:11 -0300

    Finalizado script para o projeto cotador.

--- projetos/criandoUmContador/assets/js/scripts.js
+++ projetos/criandoUmContador/assets/js/scripts.js
@@ -1,1 +1,12 @@
-.
+let count = 0;
+
+const CURRENT_NUMBER = document.getElementById('currentNumber');
+function increment() {
+    count++;
+    CURRENT_NUMBER.innerHTML = count;
+}
+
+function decrement() {
+    count--;
+    CURRENT_NUMBER.innerHTML = count;
+}
