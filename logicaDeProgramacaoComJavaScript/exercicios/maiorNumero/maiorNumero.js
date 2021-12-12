9131b73fbf74d2fa767ad9b41601a47ddc967439
Nildo <nildo@webstack.com.br>
2021-12-12 20:17:45 -0300

    Feito correção de fechamento da tag <section>

---
+++ logicaDeProgramacaoComJavaScript/exercicios/maiorNumero/maiorNumero.js
@@ -0,0 +1,69 @@
+/*
+    Escreva uma função que receba dois números e retorne o maior
+    deles
+*/
+
+// exemplo 7
+const maiorValor = (g, h) => g > h? g : h;
+console.log(maiorValor(5, 9));
+
+
+//  exemplo 6
+// const maiorValor = (i, p) => {
+//     return i > p? i : p;
+// }
+//
+// console.log(maiorValor(3, 7));
+
+
+
+// exemplo 5
+// function maiorValor(k, y) {
+//     return k > y? k : y
+// }
+//
+// console.log(maiorValor(15, 7));
+
+// exemplo 4
+// function maiorValor(p, q) {
+//     if (p > q);
+//         return p;
+//             return q;
+// }
+// console.log(maiorValor(5, 1));
+
+
+
+// exemplo 3
+// function maiorValor(m, n) {
+//     if (m > n) {
+//         return m;
+//     }
+//     return n;
+// }
+// //
+// console.log(maiorValor(2, 5));
+
+
+// exemplo 2
+// function maiorValor(a, b) {
+//     if (a > b) {
+//         return a;
+//     } else {
+//         return b;
+//     }
+// }
+//
+// console.log(maiorValor(1, 3));
+
+// exemplo 1
+// function maiorValor(x, y) {
+//     if (x > y) {
+//         return x;
+//     } else {
+//         return y;
+//     }
+// }
+//
+// const maior = maiorValor(1, 2);
+// console.log(maior);
