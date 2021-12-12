78c31a6622e6817d193b94a81c7e4f70a2d8c2b7
Nildo <nildo@webstack.com.br>
2021-12-12 13:34:05 -0300

    Arquivos do projeto atualizados.

--- integracaoHTML/formulario/js/formulario.js
+++ integracaoHTML/formulario/js/formulario.js
@@ -1,4 +1,7 @@
 function mostrarOla() {
 	var nome = document.getElementById('nome').value;
-	
+	document.getElementById('resposta').textContent = 'Olá ' + nome;
 }
+
+var mostrar = document.getElementById('mostrar');
+mostrar.addEventListener('clclick', mostrarOla);
--- logicaDeProgramacaoComJavaScript/exercicios/nodeList/js/node-list.js
+++ logicaDeProgramacaoComJavaScript/exercicios/nodeList/js/node-list.js
@@ -1,5 +1,16 @@
+// seleciona a classe paragrafos
 const paragrafos = document.querySelector('.paragrafos');
-const ps = document.querySelectorAll('p');
-for (let p of ps) {
-    console.log(p);
+
+// seleciona todas as tags p dentro da classe paragrafos
+const todosParagrafos = paragrafos.querySelectorAll('p');
+
+// seleciona a cor de fundo do body
+const selecionaCorFundoBody = getComputedStyle(document.body);
+
+// pega a cor de fundo do body
+const pegaCorFundoBody = selecionaCorFundoBody.backgroundColor;
+
+for (let paragrafo of todosParagrafos) {
+    paragrafo.style.backgroundColor = pegaCorFundoBody;
+    paragrafo.style.color = '#0e3a85';
 }
