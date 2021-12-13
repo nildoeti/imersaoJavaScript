0428ccd41b4dd755cfa31495749b8ae9fe539a78
Nildo <nildo@webstack.com.br>
2021-12-13 12:16:41 -0300

    Adicioando descrição do problema a ser resolvido.

---
+++ logicaDeProgramacaoComJavaScript/exercicios/numDivisivel/numDivisivel.js
@@ -0,0 +1,11 @@
+/*
+    Escreva uma função que receba um número e retorne o seguinte:
+
+    Número divisível por 3? Retorna Fizz.
+    Número divisível por 3? Retorna Buzz.
+    Número divisível por 3 e 5? FizzBuzz.
+    Número não é divisível por 3 e 5? Retornar o próprio número.
+
+    Checar se o número é realmente um número.
+    Use a função com números de 0 - 100.
+*/
