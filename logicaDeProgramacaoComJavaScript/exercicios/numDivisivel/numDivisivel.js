a27bf8d250f81a97fb4491b14dbc8737ecafc46f
Nildo <nildo@webstack.com.br>
2021-12-13 22:40:13 -0300

    Finalizado solução para o exercício.

--- logicaDeProgramacaoComJavaScript/exercicios/numDivisivel/numDivisivel.js
+++ logicaDeProgramacaoComJavaScript/exercicios/numDivisivel/numDivisivel.js
@@ -2,10 +2,23 @@
     Escreva uma função que receba um número e retorne o seguinte:
 
     Número divisível por 3? Retorna Fizz.
-    Número divisível por 3? Retorna Buzz.
+    Número divisível por 5? Retorna Buzz.
     Número divisível por 3 e 5? FizzBuzz.
     Número não é divisível por 3 e 5? Retornar o próprio número.
 
     Checar se o número é realmente um número.
     Use a função com números de 0 - 100.
 */
+
+function fizzBuzz(numero) {
+    if (typeof(numero) !== 'number') return 'Não é um número.'
+    if (numero %3 === 0 && numero %5 === 0) return 'FizzBuzz';
+    if (numero %3 === 0) return 'Fizz';
+    if (numero %5 === 0) return 'Buzz';
+    return numero;
+}
+
+console.log('a', fizzBuzz('a'));
+for (let i = 0; i <= 100;i++) {
+    console.log(i, fizzBuzz(i));
+}
