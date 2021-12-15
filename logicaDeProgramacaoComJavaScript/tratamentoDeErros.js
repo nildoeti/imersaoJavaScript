c7923c616fb6a89ea14efbad7a445165480719c5
Nildo <nildo@webstack.com.br>
2021-12-15 15:10:23 -0300

    tratamentoDeErros.js foi atualizado.

--- logicaDeProgramacaoComJavaScript/tratamentoDeErros.js
+++ logicaDeProgramacaoComJavaScript/tratamentoDeErros.js
@@ -13,19 +13,38 @@
 
 // exemplo 2
 
-function sum(x, y) {
-    // função para teste se é um numeral, e se não for gerar erro...
-    if (typeof(x) !== 'number' || typeof(y) !== 'number') {
-        throw new RangeError('x e y, not are numbers.')
+// function sum(x, y) {
+//     // função para teste se é um numeral, e se não for gerar erro...
+//     if (typeof(x) !== 'number' || typeof(y) !== 'number') {
+//         throw new RangeError('x e y, not are numbers.')
+//     }
+
+//     return x + y;
+// }
+
+// try {
+
+//     console.log(sum(2, 4));
+//     console.log(sum(2, '4'));    
+// } catch(error) {
+//     console.log(error);
+// }
+
+
+
+
+// exemplo 3
+notNumber = (a, b) => {
+    if (typeof(a) !== 'number' || typeof(b) !== 'number') {
+        throw new Error('Not is a number.')
     }
 
-    return x + y;
+    return a * b;
 }
 
 try {
-
-    console.log(sum(2, 4));
-    console.log(sum(2, '4'));    
-} catch(error) {
+    console.log(notNumber(2, '5'));
+    console.log(notNumber(2, 54));
+} catch (error) {
     console.log(error);
 }
