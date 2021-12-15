bc12557922b2e55073269d2468c6d02b64892deb
Nildo <nildo@webstack.com.br>
2021-12-15 14:42:12 -0300

    tratamentoDeErros.js foi atualizado.

---
+++ logicaDeProgramacaoComJavaScript/tratamentoDeErros.js
@@ -0,0 +1,31 @@
+// console.log(naoExisto); // ReferenceError: naoExisto is not defined
+
+// exemplo 1
+
+// try {
+
+//     console.log(naoExisto); 
+// } catch(error) {
+//     console.log('naoExisto não esta definida como uma variável');
+//     console.log(error);
+// }
+
+
+// exemplo 2
+
+function sum(x, y) {
+    // função para teste se é um numeral, e se não for gerar erro...
+    if (typeof(x) !== 'number' || typeof(y) !== 'number') {
+        throw new RangeError('x e y, not are numbers.')
+    }
+
+    return x + y;
+}
+
+try {
+
+    console.log(sum(2, 4));
+    console.log(sum(2, '4'));    
+} catch(error) {
+    console.log(error);
+}
