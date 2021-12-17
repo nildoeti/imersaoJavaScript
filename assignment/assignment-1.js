3bc7a5be4897ddc775d95e0555f67830cfd9afb2
Nildo <nildo@webstack.com.br>
2021-12-17 08:54:57 -0300

    assignment-1.js it has been updated

---
+++ assignment/assignment-1.js
@@ -0,0 +1,24 @@
+let x = 2;
+let y = 3;
+
+// assignmet
+// console.log(x = y); // return 3
+
+// addition assignment
+// console.log(x = x + y); // return 5
+
+// subtraction assignment
+// console.log(x = x - y); // return -1
+// console.log(x -= y); // return -1
+
+// multiplication assignment
+// console.log(x = x * y); // return 6
+// console.log(x *= y); // return 6
+
+// divison assignment
+// console.log(x = x / y); // return 6666666666666666
+// console.log(x /= y); // 
+
+// rest assignment
+// console.log(x = x % y); // return 2
+console.log(x %= y); // return 2
