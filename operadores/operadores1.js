38161c0fe62aa88c27a4ab759dc79e968a0826ea
Nildo <nildo@webstack.com.br>
2021-12-17 07:18:21 -0300

    Finished script for operadores.js

---
+++ operadores/operadores1.js
@@ -0,0 +1,39 @@
+/*
+     JavaScript operators
+     .arithmetic
+     .assignment
+     .comparison
+     .conditional
+     .logical
+     .spread
+*/
+
+// module
+// binary operator - return the rest of the division
+let rest = 12 % 5;
+console.log(rest); // rest 2
+
+
+/* increment and decrement
+     ++x;
+     x++;
+
+     --x;
+     x--;
+*/
+// subtraction (-) and addition (+)
+console.log(-3); // return 3
+console.log(+'3'); // return 3
+console.log(+true); // return 1
+console.log(+false); // return 0
+
+
+console.log(-true); // return -1
+
+// exponentiation operator (**)
+console.log(2 ** 3); // return 8
+console.log(10 ** -1); // return 0.1
+
+// grouping operator
+console.log(2 * (3 + 2)); // return 10
+
