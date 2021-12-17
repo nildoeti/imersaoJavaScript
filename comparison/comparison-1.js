9e342938e15de6d987abbb9a907097e1bc51b87e
Nildo <nildo@webstack.com.br>
2021-12-17 09:33:16 -0300

    comparison-1.js it has been updated

---
+++ comparison/comparison-1.js
@@ -0,0 +1,31 @@
+	// equal (==) 
+	// return true if operators are equal
+	console.log(3 == 3); // return true
+
+	// it's not the same (!=)
+	// return true different operators
+	console.log(3 != 2); // return true
+
+	// strictly equal (===)
+	// true for equal and some type operators
+	console.log(3 === 3); // return true
+
+	// strictly not equal (!==)
+	// returns true if operators are not equals or of the same type
+	console.log(3 !== 3); // return false
+
+	// bigger then (>)
+	// return true for left operator 
+	console.log(4 > 2); // true
+
+	// bigger or equal
+	// 	return true if operator is greater than or  equal right to right operator
+	console.log(3 >= 2); // return true
+
+	// less then (<)
+	// return true if left operator is smaller
+	console.log(3 < 4); // true
+
+	// less than or equal (<=)
+	// return true if less operator is smaller
+	console.log(3 <= 3); // return true
