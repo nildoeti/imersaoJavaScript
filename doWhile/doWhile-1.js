15bd79e69b10a335f87623284c2fdd0467d4e3ca
Nildo <nildo@webstack.com.br>
2021-12-19 13:22:27 -0300

    doWhile-1.js foi atualizado.

---
+++ doWhile/doWhile-1.js
@@ -0,0 +1,13 @@
+/*
+	do...while
+	A declaração do...while cria um laço que executa uma declaração até que o 
+	teste da condição for falsa (false). A condição é avaliada depois que o 
+	bloco de código é executado, resultando que uma declaração seja executada 
+	pelo menos uma vez.
+*/
+
+let i = 0;
+do {
+	i += 1;
+	console.log(i);
+} while (i < 5);
