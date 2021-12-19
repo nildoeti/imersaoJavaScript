7c8b6b35548a1674e63d1c4c328370b82eb92e22
Nildo <nildo@webstack.com.br>
2021-12-18 23:52:26 -0300

    for-1.js foi atualizado.

---
+++ for/for-1.js
@@ -0,0 +1,13 @@
+const array = ['one', 'two', 'three'];
+
+for (let index = 0; index < array.length; index++) {
+	const element = array[index];
+	console.log(`Element => ${index}: ${element}.`);
+}
+
+
+const cores = ['azul', 'amarelo', 'verde', 'branco'];
+
+for (var i = cores.length - 1; i >= 0; i--) {
+	console.log(`=> ${cores[i]}`);
+};
