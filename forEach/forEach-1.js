6ba9af2bcd1bcd7d47694a7f47716013a94fb0b9
Nildo <nildo@webstack.com.br>
2021-12-20 18:03:23 -0300

    forEach-1.js foi atualizado.

---
+++ forEach/forEach-1.js
@@ -0,0 +1,16 @@
+const pets = [
+	{type: 'dog', name: 'bolinha', age: 15, weight: 30},
+	{type: 'gat', name:'mingal', age: 6, weight: 2},
+	{type: 'dog', name: 'rex', age: 4, weight: 5},
+	{type: 'gat', name: 'marron', age: 2, weight: 1},
+	{type: 'fish', name: 'gulp', age: 1, weight: 0.01},
+	{type: 'horse', name: 'pé de pano', age: 1, weight: 0.01}
+]
+
+
+console.log(pets);
+
+const forEachPetNames = []
+
+pets.forEach((pet) => {forEachPetNames.push(pet.name)});
+console.log('Nome dos animais da lista\n', forEachPetNames);
