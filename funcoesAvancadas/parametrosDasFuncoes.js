842f827bcc2cdd447a01242076a995a7c71cba63
Nildo <nildo@webstack.com.br>
2022-01-03 10:09:42 -0300

    Exemplo 2 para parâmetros da função foi atualizado

--- funcoesAvancadas/parametrosDasFuncoes.js
+++ funcoesAvancadas/parametrosDasFuncoes.js
@@ -1,9 +1,9 @@
 // EXEMPLO 1
-function minhaFuncao() {
-	console.log('OI');
-}
+// function minhaFuncao() {
+// 	console.log('OI');
+// }
 
-minhaFuncao('Valor'); // OI
+// minhaFuncao('Valor'); // OI
 
 
 // EXEMPLO 2
@@ -11,5 +11,4 @@ function minhaFuncao2() {
 	console.log(arguments);
 }
 
-// [Arguments] { '0': 'Valor', '1': 1, '2': 2, '3': 4, '4': 5, '5': 6 }
-minhaFuncao2('Valor', 1, 2, 4, 5, 6) 
+minhaFuncao2('Teste 2', 2, 3, 4, 5, 6, 7, 8, 9, 0, 'frase');
