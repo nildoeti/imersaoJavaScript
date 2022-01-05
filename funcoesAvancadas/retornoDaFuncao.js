5cd3f59b319b316a914a19f8ad080e3609121a08
Nildo <nildo@webstack.com.br>
2022-01-05 14:38:48 -0300

    Adicionado segundo exemplo para a chamada da função

--- funcoesAvancadas/retornoDaFuncao.js
+++ funcoesAvancadas/retornoDaFuncao.js
@@ -5,10 +5,19 @@
 	Fonte: MDN url(https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/return)
 */
 
-function minhaFuncao(a, b) {
-	return a + b; // retorno da função aqui
+// function minhaFuncao(a, b) {
+// 	return a + b; // retorno da função aqui
+// }
+// // chamada da função aqui com dados sendo armazenados em uma variável
+// let result = minhaFuncao(2, 4); 
+// console.log(result);
+
+// exemplo 2
+function soma2(a, b) {
+	// exemolo de função inutil
+	console.log(a + b); 
 }
-// chamada da função aqui com dados sendo armazenados em uma variável
-let result = minhaFuncao(2, 4); 
-console.log(result);
+
+soma2(4, 7); 
+
 
