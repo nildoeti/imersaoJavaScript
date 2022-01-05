df2f04294803393ce8fe0b16be2a5acabf09c5e7
Nildo <nildo@webstack.com.br>
2022-01-05 14:26:12 -0300

    Adicionado primeiro exemplo para a chamada da função

--- funcoesAvancadas/retornoDaFuncao.js
+++ funcoesAvancadas/retornoDaFuncao.js
@@ -1,1 +1,14 @@
-retornoDaFuncao.js
+/*
+	return
+	A declaração return finaliza a execução de uma função e especifica os 
+	valores que devem ser retonados para onde a função foi chamada.
+	Fonte: MDN url(https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/return)
+*/
+
+function minhaFuncao(a, b) {
+	return a + b; // retorno da função aqui
+}
+// chamada da função aqui com dados sendo armazenados em uma variável
+let result = minhaFuncao(2, 4); 
+console.log(result);
+
