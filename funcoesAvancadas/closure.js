55b23408dce3a3df8185a4920ab7edf86ee2234b
Nildo <nildo@webstack.com.br>
2022-01-05 22:17:01 -0300

    exemplo para Closure finalizado

--- funcoesAvancadas/closure.js
+++ funcoesAvancadas/closure.js
@@ -6,21 +6,42 @@
 	escopo léxico — em que ela foi criada.
 */
 
-function init() {
-	// Escopo léxico
+// exemplo para escopo léxico
+// function init() {
+// 	// Escopo léxico
+// 	var name = 'Mozilla';
+// 	function displayName() {
+// 		console.log(name);
+		
+// 			displayName() é uma função aninhada (um closure) — ela é definida 
+// 			dentro da função init(), e está disponivel apenas dentro do corpo 
+// 			daquela função. Diferente de init(), displayName() não tem variáveis 
+// 			locais próprias, e ao invés disso reusa a variável name declarada 
+// 			na função pai.
+		
+// 	}
+// 	displayName();
+// }
+// console.log(init());
+
+
+// exemplo para Closure
+function makeFun() {
 	var name = 'Mozilla';
 	function displayName() {
 		console.log(name);
-		/*
-			displayName() é uma função aninhada (um closure) — ela é definida 
-			dentro da função init(), e está disponivel apenas dentro do corpo 
-			daquela função. Diferente de init(), displayName() não tem variáveis 
-			locais próprias, e ao invés disso reusa a variável name declarada 
-			na função pai.
-		*/
 	}
-	displayName();
+	return displayName;
 }
-console.log(init());
+	/*
+		variável myFun tourna-se uma função e por consequeẽncia de sua natureza,
+		também se torna uma closure.
+		Uma closure (fechamento) trata-se de um tipo especial de objeto que combina 
+		duas coisas: a função e o ambiente onde a função foi criada.
+		myFunc é a closure que incorpora tanto a função displayName quanto a 
+		palavra Mozilla que existia quando a closure foi criada.
+	*/
+var myFun = makeFun(); 
+console.log(myFun);
 
 
