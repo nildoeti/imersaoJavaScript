08899d7832498e8525e3a70764dbc2838e8e00c0
Nildo <nildo@webstack.com.br>
2022-01-17 15:58:23 -0500

    /super/super.js ao projeto

---
+++ super/super.js
@@ -0,0 +1,20 @@
+/*
+	super
+
+	A palavra-chave super é usada para acessar o objeto pai de um objeto, em
+	outros casos, é usada para acessar a classe pai de uma classe.
+	Síntaxe
+
+	// chama o objeto (ou construtor) pai
+	super(...[arguments]);
+	// chama um método da classe/objeto pai
+	super.metodo([arguments]);
+
+	Descrição
+
+	Quando usada no construtor de uma classe, a palavra-chave super deve ser
+	usada apenas uma vez, e precisa ser usada antes que a palavra-chave this
+	possa ser usada. Essa palavra-chave também pode ser usada para chamar uma
+	função ou objeto pai.
+*/
+
