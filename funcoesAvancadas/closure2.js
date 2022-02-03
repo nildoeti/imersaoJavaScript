7d0cf0c4c524c356b93c59e95f173e9e6a6f67c0
Nildo <nildo@webstack.com.br>
2022-02-03 09:53:25 -0500

    adicionado exemplo 2 para closure

--- funcoesAvancadas/closure2.js
+++ funcoesAvancadas/closure2.js
@@ -1,6 +1,19 @@
 // exemplo 1
+// function retornaFuncao() {
+// 	const nome = 'Fulano';
+// }
+// console.log(retornaFuncao()); // undefined
+
+
+// exemplo 2
+// escopo global
 function retornaFuncao() {
 	const nome = 'Fulano';
-}
-console.log(retornaFuncao()); // undefined
 
+	// retornando uma função anônima
+	return function() {
+		return nome;
+	}
+}
+console.log(retornaFuncao()); // [Function (anonymous)]
+console.dir(retornaFuncao()); // [Function (anonymous)]
