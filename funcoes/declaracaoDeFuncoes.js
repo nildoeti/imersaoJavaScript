03c8c630ac3d7ba0b2a709787aadb86ec7bcbd8f
Nildo <nildo@webstack.com.br>
2022-02-09 11:45:02 -0300

    adicionado função que retorna a quantidade total das   vendas ao projeto

--- funcoes/declaracaoDeFuncoes.js
+++ funcoes/declaracaoDeFuncoes.js
@@ -10,8 +10,22 @@ function hoisted() {
 };
 
 // Note que function expressions não são hoisted:
-noHosted(); // TypeError: noHosted is not a function
+// noHosted(); // TypeError: noHosted is not a function
+//
+// var noHosted = function() {
+//     console.log('bar');
+// };
 
-var noHosted = function() {
-    console.log('bar');
-};
+
+/*
+    Usando funções
+    O seguinte código declara uma função que retorna a quantidade total das
+    vendas, quando dados os números de unidades vendidas dos produtos a, b e c.
+*/
+
+function calc_sales(units_a, units_b, units_c){
+    return units_a * 79 + units_b * 129 + units_c * 699;
+}
+
+const result = calc_sales(2, 4, 6);
+console.log(result); // 4868
