e21d9911434aa081a2e8085ca9697a98ed0d2c30
Nildo <nildo@webstack.com.br>
2022-02-09 12:07:32 -0300

    addd short function two in project

--- funcoesAvancadas/funcoesDeFabrica.js
+++ funcoesAvancadas/funcoesDeFabrica.js
@@ -1,11 +1,24 @@
 // Factory function (Função fábrica)
 // Constructor function (Função construtora)
 
-function criaPessoa(nome, sobreNome){
-    return {
-        nome, sobreNome
-    };
-}
+// exemplo 1 para factory function
+// function criaPessoa(nome, sobreNome){
+//     return {
+//         nome,
+//         sobreNome,
+//     };
+// }
+//
+// const p1 = criaPessoa('Fulano', 'de Tal');
+// console.log(p1);
 
-const p1 = criaPessoa('Fulano', 'de Tal');
-console.log(p1);
+// exemplo 2 para factory function
+function criaPessoa(nome, sobreNome, a, p) {
+    return {
+        nome,
+        sobreNome,
+
+        // Getter
+        get nomeCompleto()
+    }
+}
