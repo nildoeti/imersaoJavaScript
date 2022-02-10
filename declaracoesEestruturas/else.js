1e087ee3b9e9be19874086677b339aea912eb2a1
Nildo <nildo@webstack.com.br>
2022-02-10 17:54:04 -0300

    adicionado exemplo para a declaração else

---
+++ declaracoesEestruturas/else.js
@@ -0,0 +1,14 @@
+/*
+ * A declaração else é usada em conjunto com a declaração if
+ * permitindo uma tomade de decisão quando a condição for falsa
+ * 
+ * */
+ 
+ 
+ let idade = 28;
+ if (idade <= 18)
+ {
+     console.log('Você é menor de idade');
+ } else {
+     console.log('Você é adulto');
+ }
