8f88c74ffe76dd065fe7724654b11302b918fa90
Nildo <nildo@webstack.com.br>
2022-02-10 14:01:51 -0300

    adicionado descrições para declarações em JavaScript

---
+++ declaracoesEestruturas/declaracoes.js
@@ -0,0 +1,26 @@
+/*
+	Declarações
+	São linhas e/ou comandos que compõe um script.
+
+	Declarações			Finalidade
+	var 				Declara uma variável
+	function 			Declara uma função
+	return 				Retorna um valor
+	if/else 			Cria estrutura condicional
+	switch 				Cria estrutura condicional
+	case 				Usado em estrutura condicional switch
+	break 				Usado em estrutura condicional switch
+	default 			Usado em estrutura condicional switch
+	for 				Criar um laço
+	continue 			Reinicia um loop
+	while 				Cria estrutura de repetição
+	do/while 			Cria estrutura de repetição
+	for/in 				Cria loop em objeto
+	throw 				Sinaliza erros
+	try/cat/finaly		Trata erros
+	with 				Altera o escopo
+	; 					Declaração vazia
+
+	Todo os arquivos presentes neste diretório são retirados do livro,
+	Java Script Guia do Programador, 92-118, Maurício Samy Silva
+*/
