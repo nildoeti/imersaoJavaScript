822402c1a51b6e23eff91bdaf6f3874897ab3502
Nildo <nildo@webstack.com.br>
2022-02-10 20:46:44 -0300

    adicionado exemplo prático para switch

---
+++ declaracoesEestruturas/switch.js
@@ -0,0 +1,25 @@
+/*
+    SWITCH
+    A condicional switch avalia uma expressão, combinando o valor da expressão
+    para um cláusula case, e executa as instruções  associadas ao case.
+    Usar switch/case sempre que possível para ganhar em eficiência e
+    performance, se a condicional for a mesma
+*/
+
+// exemplo prático
+var estadoSudeste = 'SP';
+
+switch (estadoSudeste) {
+    case 'MG':
+        console.log('Minas Gerais');
+        break;
+    case 'RJ':
+        console.log('Rio de Janeiro');
+        break;
+    case 'SP':
+        console.log('São Paulo');
+        break;
+    default:
+        console.log('opção não identificada');
+
+}
