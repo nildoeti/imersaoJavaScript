3fbb37c60d86281e2d766b8a09d7eebfbc7fd0a2
Nildo <nildo@webstack.com.br>
2022-02-10 17:36:51 -0300

    adicionado exemplo para return

---
+++ declaracoesEestruturas/return.js
@@ -0,0 +1,16 @@
+/*
+	A declaração return finaliza a execução de uma função e especifica os
+	valores que devem ser retonados para onde a função foi chamada.
+*/
+
+function soma(x, y) {
+	return x + y;
+}
+
+let resultado1 = soma(2, 1);
+let resultado2 = soma(2, 2);
+let resultado3 = soma(2, 3);
+
+console.log(resultado1);
+console.log(resultado2);
+console.log(resultado3);
