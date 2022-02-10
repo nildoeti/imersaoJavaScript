f1a7f234383e32f0fdc3f1dba7cf189739e05a94
Nildo <nildo@webstack.com.br>
2022-02-10 14:08:35 -0300

    adicionado exemplos para a criação de variáveis com a palavra chave var

---
+++ declaracoesEestruturas/var.js
@@ -0,0 +1,10 @@
+// palavra chave var serve para declarar uma ou mais variáveis
+var x; // declara uma varipavel de nome x
+x = 6; // atribui um valor para a variável x
+console.log(x) ; // exibe o valor da variável em um terminal
+
+var y = 9; // declara e inicializa a variável y
+var soma = x + y; // declara e inicializa a variável soma com a adição dos valores
+					// das variáveis x e y
+console.log(soma); // exibe o valor da variável soma
+
