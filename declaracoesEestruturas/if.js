74095f21e8e296b2dae52fb05f0cd24290fa070f
Nildo <nildo@webstack.com.br>
2022-02-10 17:44:31 -0300

    adicinado exemplo prático para condifição if

--- declaracoesEestruturas/if.js
+++ declaracoesEestruturas/if.js
@@ -1,1 +1,12 @@
+/*
+ * IF é uma estrutura de testes em que uma determinada condição
+ * quando satisfeira, retorna um valor
+ */
+ 
+ // exemplo prático em caso de condição for true, verdadeira
+ let idade = 3;
+ if (idade <= 10)
+ {
+     console.log('Você é uma criança');
+ }
 
