8f76eaa4c5d2e9a11abbf7de497d0154bce51c18
Nildo <nildo@webstack.com.br>
2022-02-10 16:13:48 -0300

    adicionado exemplos prático para uso de funções

---
+++ declaracoesEestruturas/function.js
@@ -0,0 +1,13 @@
+/*
+	Funções são blocos de instruções com objetivos variados como
+	executar tarefas e/ou calculos.
+*/
+
+// exemplo de uma função que multiplica dois números e retorna seu valor
+function multiplica (x, y) {
+	let resutado = x * y;
+	console.log(x + ' x ' + y + ' = ' + resutado);
+}
+multiplica(2, 1); // 2 x 1 = 2
+multiplica(2, 2); // 2 x 2 = 4
+multiplica(2, 3); // 2 x 3 = 6
