2a6b528b906c0ccc5136891855ee26e86cc9b4cb
Nildo <nildo@webstack.com.br>
2022-02-18 17:36:43 -0300

    solução para o desafio 3 do projeto foi finalizada

--- declaracoesEestruturas/estrutura-for.js
+++ declaracoesEestruturas/estrutura-for.js
@@ -14,7 +14,7 @@
 // console.log(typeof(msg));
 
 
-// Desafio
+// Desafio 1
 // Criar uma solução para que a vírgula ao final do ultímo digito, não apareça
 
 // exemplo 1 do desafio
@@ -30,16 +30,34 @@
 // resultado não esperado
 // console.log(msg); // 0, 1, 2, 3, 4, 5, 6, 7, 8, 99,
 
-// Desafio
+
+// Desafio 2
 // Continuação para o desafio acima
+// let msg = '';
+
+// for (let i = 0; i < 10; i++) {
+//     if (i == 9) {
+//         msg += i;
+//         break;
+//     };
+//     msg += i + ', ';
+// }
+
+// console.log(msg); // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+
+
+// Desafio 3
+// Continuação do exemplo acima, porém retorna os números pares
 let msg = '';
 
-for (let i = 0; i < 10; i++) {
-    if (i == 9) {
-        msg += i;
-        break;
+for (let i = 0; i <= 20; i++) {
+    if (i % 2 === 0) {
+        if (i == 20) {
+            msg += i;
+            break;
+        };
+        msg += i + ', ';
     };
-    msg += i + ', ';
 }
 
-console.log(msg); // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+console.log(msg);
