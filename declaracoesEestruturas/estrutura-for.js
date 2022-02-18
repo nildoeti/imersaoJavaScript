8cfeff92ef0dcea716954e378b4fc18f949e6d1e
Nildo <nildo@webstack.com.br>
2022-02-18 16:18:02 -0300

    resultado não esperado para o desafio

--- declaracoesEestruturas/estrutura-for.js
+++ declaracoesEestruturas/estrutura-for.js
@@ -1,14 +1,31 @@
 /*
     LAÇO FOR
-    A declaração for, cria um loop, em que testa uma condição e executa um 
+    A declaração for, cria um loop, em que testa uma condição e executa um
     determinado enquanto a condição satisfazer uma determinada condição
     pré estabelecido pelo programador.
 */
 
 // exemplo prático 1
+// let msg = '';
+// for (let i = 0; i < 10; i++) {
+//     msg += i + ', ';
+// }
+// console.log(msg);
+// console.log(typeof(msg));
+
+
+// Desafio
+// Criar uma solução para que a vírgula ao final do ultímo digito, não apareça
+
+// exemplo 1 do desafio
 let msg = '';
+
 for (let i = 0; i < 10; i++) {
+    if (i == 9) {
+        msg += i;
+    };
     msg += i + ', ';
-}
-console.log(msg);
-console.log(typeof(msg));
+};
+
+// resultado não esperado
+console.log(msg); // 0, 1, 2, 3, 4, 5, 6, 7, 8, 99,
