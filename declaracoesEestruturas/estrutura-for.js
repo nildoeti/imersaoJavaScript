fc7458ccfacabd3e230edfa0a4aa4b03b11a68f4
Nildo <nildo@webstack.com.br>
2022-02-18 09:17:34 -0300

    exemplo prático 1 adicionado ao projeto

---
+++ declaracoesEestruturas/estrutura-for.js
@@ -0,0 +1,14 @@
+/*
+    LAÇO FOR
+    A declaração for, cria um loop, em que testa uma condição e executa um 
+    determinado enquanto a condição satisfazer uma determinada condição
+    pré estabelecido pelo programador.
+*/
+
+// exemplo prático 1
+let msg = '';
+for (let i = 0; i < 10; i++) {
+    msg += i + ', ';
+}
+console.log(msg);
+console.log(typeof(msg));
