266c97574d6169118eee167e9aa610de57df11a0
Nildo <nildo@webstack.com.br>
2022-02-21 08:55:57 -0300

    revisado exemplos para o método create() com aplicação em protótipos de objectos

--- objectsPrototype/prototipoObjetos.js
+++ objectsPrototype/prototipoObjetos.js
@@ -42,3 +42,20 @@ console.log(Object.prototype); // [Object: null prototype] {}
 // exemplos de herança de cadeia de protótipos
 let myString = 'This is my string';
 // myString.slice(0, 3); // exemplo de métodos disponílve 
+
+
+// revisando o método create()
+let person2 = Object.create(person1); // cria um novo objeto a partir de um 
+                                      // object existente - prototipo existente
+
+console.log('---', person2.__proto__); // Person {first: 'Bob', last: 'Smith', age: 32, interests: Array(2)}
+
+
+// colocado parênteses na propriedade do constructor
+// incluir a palavra-chave new para especificar que deseja usar a função como um construtor.
+let person3 = new person1.constructor('Karen', 'Stephenson', 26, 'femele', ['playing drums', 'moutain climbing']);
+// console.log(person3); 
+console.log(person3.age); // 26
+console.log(person3.first); // Karen
+console.log(person1.constructor.name); // retorna o nome do constructor
+
