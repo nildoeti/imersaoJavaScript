1b44852281f2f1379afdb123aa1c7e75c93ade10
Nildo <nildo@webstack.com.br>
2022-02-23 09:35:19 -0300

    adicionado exemplo de comparação de um laço for pegando os valores de um array e exemplificando o mesmo exemplo com o método filter

--- desmitificandoMapFilterReduce/exemplo-1.js
+++ desmitificandoMapFilterReduce/exemplo-1.js
@@ -1,1 +1,30 @@
 // revendo laço for
+
+var arr = [2, 3, 4, 5, 6, 7, 8]; // array populado
+var models = []; // array não populado
+
+for (var i = 0; i < arr.length; i++) {
+	if (arr.indexOf(arr[i]) === i) {
+	/*
+		O método indexOf() retorna o primeiro índice em que o elemento pode ser
+		encontrado no array, retorna -1 caso o mesmo não esteja presente.
+		Fonte MDN:
+		url(https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf)
+	*/
+		models.push(arr[i]);
+	}
+}
+console.log(models);
+console.log(arr.indexOf(2)); // 0
+console.log(arr.indexOf(3)) ; // 1
+
+// com filter
+var novoArr = arr.filter(function(elemento, i, arr){
+	// O método filter() cria um novo array com todos os elementos que passaram
+	// no teste implementado pela função fornecida.
+	return arr.indexOf(elemento) === i;
+});
+
+console.log(novoArr);
+
+
