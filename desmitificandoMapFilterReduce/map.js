27365c78585afe22db3dae90ecf36df460ad2525
Nildo <nildo@webstack.com.br>
2022-03-09 19:22:20 -0300

    map.js adicionado ao projeto

---
+++ desmitificandoMapFilterReduce/map.js
@@ -0,0 +1,14 @@
+/*
+    Map
+    O objeto Map contém pares de chave-valor e lembra a ordem original da
+    inserção das chaves. Qualquer valor (objetos e valores primitivos) podem
+    ser usados como chave ou valor.
+
+
+    Sintaxe
+    var newArray = arr.map(calback[, thisArg])
+    Fonte MDN: url(https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Map)
+
+*/
+
+
