4f179e9712a77da49afb4818a63e7cefc51cef39
Nildo <nildo@webstack.com.br>
2022-03-10 11:33:16 -0300

    adicioando exemplo para total do dogs

--- desmitificandoMapFilterReduce/encadeamento_MapFilterReduce.js
+++ desmitificandoMapFilterReduce/encadeamento_MapFilterReduce.js
@@ -6,6 +6,12 @@ const pets = [ // um array com uma lista de objetos
     weight: 0.22
 },
 {
+    name: 'Tatus',
+    type: 'dog',
+    age: 4,
+    weight: 1.5
+},
+{
     name: 'bolinha',
     type: 'cat',
     age: 2,
@@ -25,12 +31,28 @@ const pets = [ // um array com uma lista de objetos
 }
 ];
 
+// soma dos Weght dos pets
+// let totalWeight = pets.reduce( (total, pet) => {
+//     return total + pet.weight;
+// }, 0);
 
-let totalWeight = pets.reduce( (total, pet) => {
-    return total + pet.weight;
+// console.log(totalWeight); // 1.24
+
+
+// total para dogs
+let dog = pets.filter( (pet) => {
+    return pet.type === 'dog';
+});
+
+console.log(dog); 
+/*
+    Saida para dog
+    { name: 'rex', type: 'dog', age: 10, weight: 0.22},
+    { name: 'Tatus', type: 'dog', age: 4, weight: 1.5 }
+*/ 
+
+let totalWightDogs = pets.reduce( (total, dog) => {
+    return total + dog.weight;
 }, 0);
 
-console.log(totalWeight); // 1.24
-
-
- 
+console.log('Total da alturas dos dogs: ', totalWightDogs); // Total da alturas dos dogs:  2.7399999999999993
