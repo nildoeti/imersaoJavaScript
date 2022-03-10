08c6c6c035d8f426c8a2c56a7a1f1887cc29bfcb
Nildo <nildo@webstack.com.br>
2022-03-10 11:18:44 -0300

    adicionado exmplo para soma de wight dos pets

--- desmitificandoMapFilterReduce/encadeamento_MapFilterReduce.js
+++ desmitificandoMapFilterReduce/encadeamento_MapFilterReduce.js
@@ -1,1 +1,36 @@
-encadeamento_MapFilterReduce.js
+const pets = [ // um array com uma lista de objetos 
+{
+    name: 'rex',
+    type: 'dog',
+    age: 10,
+    weight: 0.22
+},
+{
+    name: 'bolinha',
+    type: 'cat',
+    age: 2,
+    weight: 1
+},
+{
+    name: 'gup',
+    type: 'fish',
+    age: 1,
+    weight: 0.01
+},
+{
+    type: 'horse',
+    name: 'pé de pano',
+    age: 1,
+    weight: 0.01
+}
+];
+
+
+let totalWeight = pets.reduce( (total, pet) => {
+    return total + pet.weight;
+}, 0);
+
+console.log(totalWeight); // 1.24
+
+
+ 
