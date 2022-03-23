d0393d6fc04e1b540e06f1a612e1b99ba41638fb
Nildo <nildo@webstack.com.br>
2022-03-23 19:57:50 -0300

    exemplo 2 adicionado ao projeto

--- funcoesRecursivas/funcoesRecursivas.js
+++ funcoesRecursivas/funcoesRecursivas.js
@@ -1,16 +1,24 @@
 /*
     funções recursivas, são funções que chamam a si próprio
+    Uma função recursiva é uma função que chama a si mesma até encontrar uma
+    instrução de parar. Essa técnica é chamada de recursão.
+
 
     Fonte: URL(https://ricardo-reis.medium.com/fun%C3%A7%C3%B5es-recursivas-no-javascript-13a1fc099c9f)
     fONTE URL(https://www.javascripttutorial.net/javascript-recursive-function/)
 */
+// exemplo 1
+// function recurse() {
+//     // ..
+//     recurse(); // invocando a si próprio no seu rdvopo
+//     // ..
+// }
 
+// exemplo 2
+function recurse() {
+    if (condition) {
 
-function recursiva(max) {
-    console.log(max);
-    if (max > 10) return;
-    max++;
-    recursiva(max);
+    } else {
+        recurse();
+    }
 }
-
-recursiva(9);
