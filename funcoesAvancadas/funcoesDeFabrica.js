449b1618b9fd8de86bcd1e5291027af6ee98844e
Nildo <nildo@webstack.com.br>
2022-03-22 22:30:37 -0300

    finalizado exemplo do projeto

--- funcoesAvancadas/funcoesDeFabrica.js
+++ funcoesAvancadas/funcoesDeFabrica.js
@@ -1,24 +1,43 @@
 // Factory function (Função fábrica)
 // Constructor function (Função construtora)
-
-// exemplo 1 para factory function
-// function criaPessoa(nome, sobreNome){
-//     return {
-//         nome,
-//         sobreNome,
-//     };
-// }
-//
-// const p1 = criaPessoa('Fulano', 'de Tal');
-// console.log(p1);
-
-// exemplo 2 para factory function
-function criaPessoa(nome, sobreNome, a, p) {
+function criaPessoa(nome, sobrenome, a, p) {
     return {
         nome,
-        sobreNome,
+        sobrenome,
 
-        // Getter
-        get nomeCompleto()
-    }
+        // GETTER
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`;
+        },
+
+        // SETTER
+        set nomeCompleto(valor) {
+            valor = valor.split(' ');
+            this.nome = valor.split();
+            this.sobrenome = valor.join(' ');
+        },
+
+        fala(assunto = 'falando sobre NADA') {
+            return `${tis.nome} está ${assunto}`;
+        },
+
+        altura: a,
+        peso: p,
+
+        // GETTER
+        get imc() {
+            const indice = this.peso / (this.altura ** 2);
+            return indice.toFixed(2);
+        }
+    };
 }
+
+
+const p1 = criaPessoa('Fulano', 'de Tal', 1.8, 80);
+console.log(p1.imc);
+
+const p2 = criaPessoa('Beltrano', 'de Tal', 1.90, 57);
+console.log(p2.imc);
+
+const p3 = criaPessoa('Ciclano', 'de Tal', 1.5, 110);
+console.log(p3.imc);
