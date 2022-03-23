ee9a85050e9607eb0aee9bc303ac81fd37bb3729
Nildo <nildo@webstack.com.br>
2022-03-23 09:18:57 -0300

    Commit teste para Atom

---
+++ constructorFunctions/constructorFunctions.js
@@ -0,0 +1,8 @@
+/*
+    function contructor => object
+    factory function =>  object
+
+    Fonte: MDN url(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/constructor)
+*/
+
+// exemplo 1
