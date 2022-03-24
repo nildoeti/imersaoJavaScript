2d157f5f4bc2f2bceb20ff2388b18684e2d09593
Nildo <nildo@webstack.com.br>
2022-03-24 14:49:26 -0300

    exemplo 2 para função geradora adicionado ao projeto

--- funcaoGeradoras/funcaoGeradoras.js
+++ funcaoGeradoras/funcaoGeradoras.js
@@ -7,15 +7,32 @@
     Fonte MDN url(https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/function*)
 */
 
-// exemplo de função geradora
+// exemplo 1 de função geradora
 function* fnGeradora(i) {
     yield i;
     yield i + 10;
-    yield i = 20;
+    yield i + 30;
+    yield i + 60;
 }
 
-let fnGen = fnGeradora(10);
+let fnGen = fnGeradora(5);
 
-console.log(fnGen.next().value);
-console.log(fnGen.next().value);
-console.log(fnGen.next().value);
+console.log(fnGen.next().value); // 5
+console.log(fnGen.next().value); // 15
+console.log(fnGen.next().value); // 35
+console.log(fnGen.next().done); // retorna true, pois a função geradora produziu seu ultimo valor
+
+
+// exemplo 2 simples
+function* idMaker () {
+    let index = 0;
+    while(true) 
+        yield index++;
+}
+
+let fnGen2 = idMaker();
+
+console.log(fnGen2.next().value); // 0
+console.log(fnGen2.next().value); // 1
+console.log(fnGen2.next().value); // 2
+console.log(fnGen2.next().value); // 3
