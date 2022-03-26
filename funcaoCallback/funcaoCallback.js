6ebb552d784e7056d031ae0559731e0ac88d215c
Nildo <nildo@webstack.com.br>
2022-03-26 10:42:49 -0300

    exemplo 1 adicionado ao projeto

--- funcaoCallback/funcaoCallback.js
+++ funcaoCallback/funcaoCallback.js
@@ -6,5 +6,26 @@
 	rotina ou ação.
 
 	Fonte MDN url(https://developer.mozilla.org/pt-BR/docs/Glossary/Callback_function)
+	Fonte WIKIPEDIA url(https://pt.wikipedia.org/wiki/Callback)
+	Fonte FREECODECAMP url(https://www.freecodecamp.org/news/javascript-callback-functions-what-are-callbacks-in-js-and-how-to-use-them/)
 */
+
+
+// exemplo 1
+function print(callback) {
+	callback();	// A função print( ) recebe outra função como parâmetro e a chama dentro dela. 
+				//  isso se chama retorno do chamado (callback)
+}
+
+function tarefaUm() {
+	// body...
+	console.log('tarefa 1');
+}
+
+function tarefaDois() {
+	console.log('Tarefa 2');
+}
+
+setTimeout(tarefaUm, 2000); 
+setTimeout(tarefaDois, 1000)
 
