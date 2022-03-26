64fa97d3b4f0dd71476463c5ff7e0be61af72cee
Nildo <nildo@webstack.com.br>
2022-03-26 09:26:48 -0300

    funcaoCallback.js foi adiconado ao projeto

---
+++ funcaoCallback/funcaoCallback.js
@@ -0,0 +1,10 @@
+/*
+	Função Callback
+
+	Uma função callback é uma função passada a outra função como argumento, que
+	é então invocado dentro da função externa para completar algum tipo de
+	rotina ou ação.
+
+	Fonte MDN url(https://developer.mozilla.org/pt-BR/docs/Glossary/Callback_function)
+*/
+
