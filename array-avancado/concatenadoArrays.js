160a199176f872a3040f4001ff49b2b4dafd9b44
Nildo <nildo@webstack.com.br>
2022-03-26 08:15:11 -0300

    filters-filtrando-arrays.js foi adicionado ao projeto

--- array-avancado/concatenadoArrays.js
+++ array-avancado/concatenadoArrays.js
@@ -13,3 +13,23 @@ const a2 = [4, 5, 6]
 const a3 = a1.concat(a2);
 console.log(a3); // [ 1, 2, 3, 4, 5, 6 ]
 
+
+/*
+	Parâmetros Rest
+
+	A sintaxe de rest parameter (parâmetros rest)  nos permite representar um
+	número indefinido de argumentos como um array.
+	
+	Sintaxe
+
+	function(a, b, ...theArgs) {
+	  // ...
+	}
+
+
+	Fonte MDN url(https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Functions/rest_parameters)
+*/ 
+
+const a4 = [...a1, 'Teste' ...a2, ...[7, 8, 9, 0]]
+console.log(a4); // [ 1, 2, 3, 4, 5, 6 ]
+
