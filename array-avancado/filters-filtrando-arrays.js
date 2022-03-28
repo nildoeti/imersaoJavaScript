dd2080817bf00aaf188d86892e0350beae528995
Nildo <nildo@webstack.com.br>
2022-03-28 10:38:39 -0300

    adicionado exemplo 2 ao projeto

--- array-avancado/filters-filtrando-arrays.js
+++ array-avancado/filters-filtrando-arrays.js
@@ -7,19 +7,33 @@
 // retornando números maior que 10
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]; // array de númros
 
-function callbackFilter(valor, indice, array) {
-                        // valor - representa cada númro do array
-                        // índice - representa o índice de cada elemento no array
-                        // array é o valor do array, que é o valor total dos arrays
-
-     // exemplo 1, não indicado                    
-     if (valor > 10) {
-        return true;
-     } else {
-        return false;
-     }
+// exemplo 2
+function callbackFilter(valor, indice, array) { // indce e array não sendo usados na função
+    return valor > 10; // exemplo mais adequado para este projetop
 }
-
 const numerosFiltrados = numeros.filter(callbackFilter);
 console.log(numerosFiltrados);
+
+
+
+
+
+
+
+// exemplo 1
+// function callbackFilter(valor, indice, array) {
+//                         // valor - representa cada númro do array
+//                         // índice - representa o índice de cada elemento no array
+//                         // array é o valor do array, que é o valor total dos arrays
+
+//      // exemplo 1, não indicado                    
+//      if (valor > 10) {
+//         return true;
+//      } else {
+//         return false;
+//      }
+// }
+
+// const numerosFiltrados = numeros.filter(callbackFilter);
+// console.log(numerosFiltrados);
 
