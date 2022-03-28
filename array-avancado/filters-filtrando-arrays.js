cc3ebdd209dbb9077ac6aa0766311e6559dfcda4
Nildo <nildo@webstack.com.br>
2022-03-28 10:50:03 -0300

    adicionado exemplo 4 com arrw fuction

--- array-avancado/filters-filtrando-arrays.js
+++ array-avancado/filters-filtrando-arrays.js
@@ -8,11 +8,20 @@
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]; // array de númros
 
 
-const numerosFiltrados = numeros.filter(function(valor) {
+// exemplo 5
+const numerosFiltrados = numeros.filter( (valor) => {
     return valor > 10;
-})
+});
 
 console.log(numerosFiltrados);
+
+
+// exemplo 4
+// const numerosFiltrados = numeros.filter(function(valor) {
+//     return valor > 10;
+// })
+
+// console.log(numerosFiltrados);
 
 
 
