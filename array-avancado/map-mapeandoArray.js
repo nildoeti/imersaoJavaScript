82f092c0ef4896433fa5c25b81efc09420a977e5
Nildo <nildo@webstack.com.br>
2022-03-28 17:04:42 -0300

    exemplo para numeros.map(valor

---
+++ array-avancado/map-mapeandoArray.js
@@ -0,0 +1,19 @@
+/*
+	Map
+	O objeto Map contém pares de chave-valor e lembra a ordem original da
+	inserção das chaves. Qualquer valor (objetos e valores primitivos)
+	podem ser usados como chave ou valor.
+
+	Altera os valores do array original
+
+	Fonte MDN url(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map)
+*/
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]; // array de númros
+
+// exemplo 1
+const exemplo1 = numeros.map( (valor, indice, array) => {
+	return valor;
+});
+
+console.log(exemplo1);
