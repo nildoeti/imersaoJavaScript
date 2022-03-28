35517eac4a7ae2c941c3abe36a0d97cd976535cc
Nildo <nildo@webstack.com.br>
2022-03-28 13:10:40 -0300

    Retorne todos os objetos do array

--- array-avancado/desafioArrayAvancado.js
+++ array-avancado/desafioArrayAvancado.js
@@ -1,1 +1,23 @@
-desafioArrayAvancado.js
+/*
+    Desafio
+    Retorne as pessoas que tem o nome com 7 letras ou mais
+    Retorne as pessoas com mais de 50 anos
+    Retorne as pessoas cujo nome termina com a letra e
+*/
+const pessoas = [
+    // array de objetos
+    {nome: 'Fulano', idade: 62},
+    {nome: 'Beltrano', idade: 23},
+    {nome: 'Ciclano', idade: 55},
+    {nome: 'Beldade', idade: 19},
+    {nome: 'Fulcano', idade: 32},
+    {nome: 'Citrano', idade: 47}
+];
+
+
+// Retorne as pessoas que tem o nome com 7 letras ou mais
+const nomesGrandes = pessoas.filter( (nome) => {
+	return typeof(nome); // retorna todos os objetos do array
+});
+
+console.log(nomesGrandes);
