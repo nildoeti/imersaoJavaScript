133beca02a906963a813025431e6ba7f25dfe7e1
Nildo <nildo@webstack.com.br>
2022-03-28 13:15:45 -0300

    Retorna todos os objetos do array com nomes maior ou igual a 8 caracteres

--- array-avancado/desafioArrayAvancado.js
+++ array-avancado/desafioArrayAvancado.js
@@ -1,5 +1,6 @@
 /*
     Desafio
+    Retorna todos os objetos do array
     Retorne as pessoas que tem o nome com 7 letras ou mais
     Retorne as pessoas com mais de 50 anos
     Retorne as pessoas cujo nome termina com a letra e
@@ -14,10 +15,15 @@ const pessoas = [
     {nome: 'Citrano', idade: 47}
 ];
 
-
-// Retorne as pessoas que tem o nome com 7 letras ou mais
-const nomesGrandes = pessoas.filter( (nome) => {
-	return typeof(nome); // retorna todos os objetos do array
-});
-
+// Retorna todos os objetos do array
+const nomesGrandes = pessoas.filter( (obj) => {return obj.nome.length >= 8} );
 console.log(nomesGrandes);
+
+
+
+// Retorne todos os objetos do array
+// const todosObjetos = pessoas.filter( (nome) => {
+// 	return typeof(nome); // retorna todos os objetos do array
+// });
+
+// console.log(todosObjetos);
