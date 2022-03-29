7e2d5b1472f9c09361b8d3489fda8dcd0fdec637
Nildo <nildo@webstack.com.br>
2022-03-29 11:01:19 -0300

    Finalizado desafio de adicionar uma chave id em cada objeto

--- array-avancado/desafio-map.js
+++ array-avancado/desafio-map.js
@@ -19,13 +19,30 @@ const pessoas = [
 
 // Retorne uma string com o nome da pessoa
 const nomePessoa = pessoas.map(valor => ({Nome: valor.nome}));
-
 console.log(nomePessoa); // [ 'Fulano', 'Beltrano', 'Ciclano', 'Beldade', 'Fulcano', 'Citrano' ]
 
 
  
 //  Remove apenas a chave "nome" do objeto
  const chaveNome = pessoas.map(obj => ({Idade: obj.idade}));
- 
  console.log(chaveNome); // [ 62, 23, 55, 19, 32, 47 ]
 
+// Adicione uma chave id em cada objeto
+const nomePessoaId = pessoas.map( (obj, indice) => {
+	obj.id = indice; // usando o valor do índice como valor para o id
+	return obj;
+});
+
+console.log(nomePessoaId);
+
+/*
+ * 	Saida para nomePessoaId:
+	[
+	  { nome: 'Fulano', idade: 62, id: 0 },
+	  { nome: 'Beltrano', idade: 23, id: 1 },
+	  { nome: 'Ciclano', idade: 55, id: 2 },
+	  { nome: 'Beldade', idade: 19, id: 3 },
+	  { nome: 'Fulcano', idade: 32, id: 4 },
+	  { nome: 'Citrano', idade: 47, id: 5 }
+	]
+*/
