7f099784a41f84dfeee1770e780b27e87fc97835
Nildo <nildo@webstack.com.br>
2022-03-28 21:28:33 -0300

    exemplo 2 para map em indice adicioando ao projeto

--- array-avancado/map-mapeandoArray.js
+++ array-avancado/map-mapeandoArray.js
@@ -11,9 +11,22 @@
 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]; // array de númros
 
-// exemplo 1
-const exemplo1 = numeros.map( (valor, indice, array) => {
-	return valor;
+// exemplo 2
+const numerosIndices = numeros.map((valor, indice, array) => {
+    return indice; 
 });
 
-console.log(exemplo1);
+console.log(numerosIndices);
+// exemplo 1
+// const numerosValor = numeros.map( (valor, indice, array) => {
+// 	return valor;
+// });
+
+// console.log(numerosValor);
+
+// 	exemplo para numeros.map(valor)
+// 	[
+// 	   5, 50, 80,  1,  2,  3,
+// 	   5,  8,  7, 11, 15, 22,
+// 	   27
+// 	]
