a3f699ca4abfbb9ab7f41bb5d4252a23b5cc9e11
Nildo <nildo@webstack.com.br>
2022-03-29 09:35:29 -0300

    exemplo 5 adicionado ao projeto

--- array-avancado/map-mapeandoArray.js
+++ array-avancado/map-mapeandoArray.js
@@ -11,12 +11,38 @@
 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]; // array de númros
 
-// exemplo 4
-const valorIndice = numeros.map( (valor, indice) => {
-	return `Valor: ${valor} - Indice: ${indice}`;
+// Exemplo 5
+const valorIndiceArray = numeros.map( (valor, indice, array) => {
+	return `Índice: ${indice} Valor: ${valor} Array: ${array}}`;
 });
 
-console.log(valorIndice);
+console.log(valorIndiceArray);
+
+/*
+ * 	Saida para valorIndiceArray
+	[
+	  'Índice: 0 Valor: 5 Array: 5,50,80,1,2,3,5,8,7,11,15,22,27}',
+	  'Índice: 1 Valor: 50 Array: 5,50,80,1,2,3,5,8,7,11,15,22,27}',
+	  'Índice: 2 Valor: 80 Array: 5,50,80,1,2,3,5,8,7,11,15,22,27}',
+	  'Índice: 3 Valor: 1 Array: 5,50,80,1,2,3,5,8,7,11,15,22,27}',
+	  'Índice: 4 Valor: 2 Array: 5,50,80,1,2,3,5,8,7,11,15,22,27}',
+	  'Índice: 5 Valor: 3 Array: 5,50,80,1,2,3,5,8,7,11,15,22,27}',
+	  'Índice: 6 Valor: 5 Array: 5,50,80,1,2,3,5,8,7,11,15,22,27}',
+	  'Índice: 7 Valor: 8 Array: 5,50,80,1,2,3,5,8,7,11,15,22,27}',
+	  'Índice: 8 Valor: 7 Array: 5,50,80,1,2,3,5,8,7,11,15,22,27}',
+	  'Índice: 9 Valor: 11 Array: 5,50,80,1,2,3,5,8,7,11,15,22,27}',
+	  'Índice: 10 Valor: 15 Array: 5,50,80,1,2,3,5,8,7,11,15,22,27}',
+	  'Índice: 11 Valor: 22 Array: 5,50,80,1,2,3,5,8,7,11,15,22,27}',
+	  'Índice: 12 Valor: 27 Array: 5,50,80,1,2,3,5,8,7,11,15,22,27}'
+	]
+
+*/
+// exemplo 4
+//const valorIndice = numeros.map( (valor, indice) => {
+	//return `Valor: ${valor} - Indice: ${indice}`;
+//});
+
+//console.log(valorIndice);
 /*
  * 	Saida para valorIndice
 	[
