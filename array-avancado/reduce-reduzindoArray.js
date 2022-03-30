78ac8bd77e45a82643b470ce764affbe4a28aa28
Nildo <nildo@webstack.com.br>
2022-03-30 07:42:23 -0300

    retornado a soma de todos os números de um array com reduce

--- array-avancado/reduce-reduzindoArray.js
+++ array-avancado/reduce-reduzindoArray.js
@@ -5,11 +5,19 @@
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]; // array de númros
 
 
-// exemplo4
-const total = numeros.reduce( (acumulador, valor, indice, array) => {
-    console.log(array)
-    return array;
+// exemplo 5 - some todos os números (reduce)
+const total = numeros.reduce((acumulador, valor, indice, array) => {
+   acumulador += valor;
+    return acumulador; // retorna o valor total dos valores do array
 }, 0);
+
+console.log(total);
+
+// exemplo4
+//const total = numeros.reduce( (acumulador, valor, indice, array) => {
+    //console.log(array)
+    //return array;
+//}, 0);
 
 
 // exemplo 3
