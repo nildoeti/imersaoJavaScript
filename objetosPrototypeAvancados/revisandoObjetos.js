6593215244b50cc2f3442badaeaade1db4c3c43f
Nildo <nildo@webstack.com.br>
2022-03-30 19:35:43 -0300

    exibindo todas as chaves para pessoa1 com laço for

--- objetosPrototypeAvancados/revisandoObjetos.js
+++ objetosPrototypeAvancados/revisandoObjetos.js
@@ -17,11 +17,32 @@ const pessoa1 = new Object();
 
 pessoa1.nome = 'Fulano';
 pessoa1.sobrenome = 'de Tal'
+pessoa1.idade = 30;
+
+
+// métodos dos objetos
+pessoa1.falarNome = function() { // método falanome
+    return (`${this.nome} esta falando.`); // template de estring
+};
+
+pessoa1.getDataNascimento = function() { // metodo getDataNascimento
+    const dataAtual = new Date();
+    return dataAtual.getFullYear - this.idade;
+};
+
+
+// exibindo todas as chaves para pessoa1
+for (let chave in pessoa1) {
+    console.log(chave);
+}
+
 
 // console.log(pessoa1.nome);
 // console.log(pessoa1.sobrenome);
 
 // deletando uma chave do objeto
 
-delete pessoa1.sobrenome;
-console.log(pessoa1);
+// delete pessoa1.sobrenome;
+// console.log(pessoa1);
+
+
