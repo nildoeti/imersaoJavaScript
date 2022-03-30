50bef4d04e1160815ea95c02424306cbef80eae1
Nildo <nildo@webstack.com.br>
2022-03-30 11:34:47 -0300

    adicionado exemplo  de objeto com uso do constructor

--- objetosPrototypeAvancados/revisandoObjetos.js
+++ objetosPrototypeAvancados/revisandoObjetos.js
@@ -1,15 +1,23 @@
-// exemplo de objeto
-const pessoa = {
-    nome: 'Fulano',
-    sobrenome: 'de Tal',
-    idade: 10000
-}
+// exemplo de objeto literal
+// const pessoa = {
+//     nome: 'Fulano',
+//     sobrenome: 'de Tal',
+//     idade: 10000
+// }
+// // acessando a chave modo 1
+// console.log(pessoa.nome); // notação de ponto
+// // acessando a chave modo 2
+// console.log(pessoa['nome']); // notação de colchetes
+// const chave = 'idade';
+// console.log(pessoa[chave]); // notação de colchetes com variaveis
 
-// acessando a chave modo 1
-console.log(pessoa.nome);
 
-// acessando a chave modo 2
-console.log(pessoa['nome']);
+// exemplo 2 de objeto com uso do constructor
+const pessoa1 = new Object();
 
-const chave = 'idade';
-console.log(pessoa[chave]);
+pessoa1.nome = 'Fulano';
+pessoa1.sobrenome = 'de Tal'
+
+console.log(pessoa1.nome);
+console.log(pessoa1.sobrenome);
+
