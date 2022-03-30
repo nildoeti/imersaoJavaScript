ecd79a4eecbe2445b0aa1fd9af905d5d50fc908a
Nildo <nildo@webstack.com.br>
2022-03-30 08:56:42 -0300

    filtrado números  pares

---
+++ array-avancado/filterMapReduce.js
@@ -0,0 +1,17 @@
+//  unir filter, map e rduce
+
+// Retornar a soma do dobro de todos os números pares
+// -> filtrar pares
+// -> dobrar os valores
+// -> reduzir (somar tudo)
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]; // array de números
+
+
+// -> filtrar pares
+const numerosPares = numeros.filter( function ( valor) {
+    return valor % 2 === 0;
+});
+
+console.log(numerosPares); // [ 50, 80, 2, 8, 22 ]
+
