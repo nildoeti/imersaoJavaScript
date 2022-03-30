57d7d2b6240e8d1d007731cd0e00e2534cb67c5b
Nildo <nildo@webstack.com.br>
2022-03-30 08:08:08 -0300

    retorne um array com o dobro dos valores (Map)

--- array-avancado/reduce-reduzindoArray.js
+++ array-avancado/reduce-reduzindoArray.js
@@ -2,15 +2,32 @@
  * Reduce - usada para reduzir o tamanho de um, array
  * */
 
-const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]; // array de númros
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]; // array de números
 
 
-// retorne um arrey com os pares (Filter)
-const total = numeros.reduce((acumulador, valor, indice, array) => {
-    if (valor %2 === 0) acumulador.push(valor);
+// retorne um array com o dobro dos valores (Map)
+const total = numeros.reduce((acumulador, valor) => {
+    acumulador.push(valor * 2);
     return acumulador;
-}, []);
-console.log(total); // [ 50, 80, 2, 8, 22 ]
+}, []); 
+console.log(total);
+
+/*
+    Saida para variável total
+    [
+    10, 100, 160,  2,  4,  6,
+    10,  16,  14, 22, 30, 44,
+    54
+    ]
+*/
+
+
+// retorne um arrey com os pares (Filter) - OBS: não fazer com reduce, é apenas exemplo, use filter
+// const total = numeros.reduce((acumulador, valor, indice, array) => {
+//     if (valor %2 === 0) acumulador.push(valor);
+//     return acumulador;
+// }, []);
+// console.log(total); // [ 50, 80, 2, 8, 22 ]
 
 
 // exemplo 5 - some todos os números (reduce)
