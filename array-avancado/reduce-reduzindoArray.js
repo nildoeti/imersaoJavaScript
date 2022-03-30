ab4c47a324efe0fa08af46581035f49335f72924
Nildo <nildo@webstack.com.br>
2022-03-29 21:39:39 -0300

    exemplo do acumulador  adicionado ao projeto

--- array-avancado/reduce-reduzindoArray.js
+++ array-avancado/reduce-reduzindoArray.js
@@ -1,1 +1,13 @@
+/*
+ * Reduce - usada para reduzir o tamanho de um, array
+ * */
 
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]; // array de númros
+
+
+
+// exemplo 1
+const total = numeros.reduce( (acumulador, valor, indice, array) => {
+	console.log(acumulador);
+	return acumulador; // 
+}, 0);
