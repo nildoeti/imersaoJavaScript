c37592d312b1b297d89133aa6a0af6c954d98181
Nildo <nildo@webstack.com.br>
2022-03-29 21:48:35 -0300

    adicioando exemplo para valor de reduce

--- array-avancado/reduce-reduzindoArray.js
+++ array-avancado/reduce-reduzindoArray.js
@@ -6,8 +6,15 @@ const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]; // array de númr
 
 
 
+// exemplo 2
+const total = numeros.map( (acumulador, valor, indice, array) => {
+	console.log(valor);
+	return valor;
+}, 0)
+
+
 // exemplo 1
-const total = numeros.reduce( (acumulador, valor, indice, array) => {
-	console.log(acumulador);
-	return acumulador; // 
-}, 0);
+// const total = numeros.reduce( (acumulador, valor, indice, array) => {
+// 	console.log(acumulador);
+// 	return acumulador; // 
+// }, 0);
