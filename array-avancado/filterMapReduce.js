b14e3277c5cfef47fae44398f56f352e5207803d
Nildo <nildo@webstack.com.br>
2022-03-30 09:06:02 -0300

    dobrado os valores dos números do array

--- array-avancado/filterMapReduce.js
+++ array-avancado/filterMapReduce.js
@@ -8,10 +8,16 @@
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]; // array de números
 
 
-// -> filtrar pares
-const numerosPares = numeros.filter( function ( valor) {
-    return valor % 2 === 0;
-});
+// -> dobrar os valores
+const dobrarValor = numeros.map( valor => valor * 2 );
+console.log(dobrarValor);
 
-console.log(numerosPares); // [ 50, 80, 2, 8, 22 ]
+
+
+// -> filtrar pares
+// const numerosPares = numeros.filter( function ( valor) {
+//     return valor % 2 === 0;
+// });
+
+// console.log(numerosPares); // [ 50, 80, 2, 8, 22 ]
 
