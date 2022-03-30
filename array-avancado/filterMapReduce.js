612f17f00c1b675da1627362745bc504af34faaf
Nildo <nildo@webstack.com.br>
2022-03-30 09:21:22 -0300

    finalizado desafio do projeto

--- array-avancado/filterMapReduce.js
+++ array-avancado/filterMapReduce.js
@@ -7,11 +7,27 @@
 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]; // array de números
 
+
+// Retornar a soma do dobro de todos os números pares e filtrar pares. dobrar os valores e reduzir e somar
+const total = numeros.filter( function (valor) {
+    return valor % 2 === 0;
+}).map(function (valor) {
+    return valor * 2;
+}).reduce( function (acumulador, valor) {
+    return acumulador + valor;
+});
+
+// filtrar números pares -> [ 50, 80, 2, 8, 22 ]
+// dobrar os valores dos números pares filtrados -> [ 100, 160, 4, 16, 44 ]
+console.log(total);
+
+
+
 // -> reduzir (somar tudo)
- const reduzirSomarTudo = numeros.reduce( (acumulador, valor) => {
-     return acumulador + valor;
- });
-console.log(reduzirSomarTudo);
+//  const reduzirSomarTudo = numeros.reduce( (acumulador, valor) => {
+//      return acumulador + valor;
+//  });
+// console.log(reduzirSomarTudo); // 236
 
 
 
