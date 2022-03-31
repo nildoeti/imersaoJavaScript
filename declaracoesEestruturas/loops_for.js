c4e7683e1a43002496ac7616b32be5f065d47dd1
Nildo <nildo@webstack.com.br>
2022-03-31 17:02:14 -0300

    extrair os números pares menores que 20 com o exemplo do laço for

--- declaracoesEestruturas/loops_for.js
+++ declaracoesEestruturas/loops_for.js
@@ -23,15 +23,31 @@
 
 
 // gerado novo aprimoramento para a estrutura for 
+// let msg = '';
+
+// for (let i = 0; i < 10; i++) {
+//     if (i == 9) {
+//         msg += i;
+//         break;
+//     };
+
+//     msg += i + ', ';
+// }
+
+// console.log(msg);
+
+
+// extrair os números pares menores que 20 com o exemplo do laço for
 let msg = '';
 
-for (let i = 0; i < 10; i++) {
-    if (i == 9) {
-        msg += i;
-        break;
+for (let i = 0; i <= 20; i++) {
+    if (i % 2 == 0) { // verifica quais números tem resto zero
+        if (i == 20) { // verifica 
+            msg += i;
+            break;
+        };
+        msg += i + ', ';
     };
-
-    msg += i + ', ';
-}
+};
 
 console.log(msg);
