ad669db84157af2487ad270d2433c03c863d803e
Nildo <nildo@webstack.com.br>
2022-03-31 10:27:12 -0300

    estrutura for aprimorada

--- declaracoesEestruturas/loops_for.js
+++ declaracoesEestruturas/loops_for.js
@@ -1,9 +1,22 @@
 // exemplo de uma estrutura for
+// let msg = '';
+
+// for (let i = 0; i < 10; i += 1) {
+//     msg += i + ', ';
+// }
+
+// console.log(msg);
+
+
+
+// estrutura for aprimorada
 let msg = '';
 
-for (let i = 0; i < 10; i += 1) {
-    msg += i + ', ';
+for (let i = 0; i < 10; i++) {
+   if (i === 9) {
+       msg += i;
+   };
+   msg += i + ', ';
 }
 
 console.log(msg);
-
