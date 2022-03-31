8d46c6bf40afcc990520d319e9a7e25286dd2da3
Nildo <nildo@webstack.com.br>
2022-03-30 22:04:14 -0300

    adicioando exemplo para onstructor function

--- objetosPrototypeAvancados/revisandoObjetos2.js
+++ objetosPrototypeAvancados/revisandoObjetos2.js
@@ -1,14 +1,28 @@
-// factor function - funções de fabrica
-function criaPessoa(nome, sobrenome) {
-    return {
-        nome,
-        sobrenome,
+// factor function - funções de fabrica 
+// function criaPessoa(nome, sobrenome) {
+//     return {
+//         nome,
+//         sobrenome,
 
-        get nomeCompleto() { // método
-            return `${this.nome} ${this.sobrenome}`; // template de strings
-        }
-    };
-}
-const p1 = criaPessoa('Fulano', 'de Tal');
-console.log(p1.nomeCompleto);
+//         get nomeCompleto() { // método
+//             return `${this.nome} ${this.sobrenome}`; // template de strings
+//         }
+//     };
+// }
+// const p1 = criaPessoa('Fulano', 'de Tal');
+// console.log(p1.nomeCompleto);
  
+
+
+//  constructor function
+function Pessoa (nome, sobrenome) { // parâmetros da função construtura
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+}
+
+const p1 = new Pessoa('Fulano', 'de Tal');
+const p2 = new Pessoa('Beltrano', 'de Tal');
+
+console.log(p1);
+// console.log(typeof(p1));
+console.log(p2);
