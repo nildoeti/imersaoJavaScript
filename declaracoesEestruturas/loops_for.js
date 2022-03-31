06483a11bcdbd36767682ed839a3c3d7f7aadb37
Nildo <nildo@webstack.com.br>
2022-03-31 10:40:07 -0300

    erado novo aprimoramento para a estrutura for

--- declaracoesEestruturas/loops_for.js
+++ declaracoesEestruturas/loops_for.js
@@ -5,18 +5,33 @@
 //     msg += i + ', ';
 // }
 
-// console.log(msg);
+// console.log(msg); 9,
 
 
 
 // estrutura for aprimorada
+// let msg = '';
+
+// for (let i = 0; i < 10; i++) {
+//    if (i == 9) {
+//        msg += i;
+//    };
+//    msg += i + ', ';
+// }
+
+// console.log(msg); // 99
+
+
+// gerado novo aprimoramento para a estrutura for 
 let msg = '';
 
 for (let i = 0; i < 10; i++) {
-   if (i === 9) {
-       msg += i;
-   };
-   msg += i + ', ';
+    if (i == 9) {
+        msg += i;
+        break;
+    };
+
+    msg += i + ', ';
 }
 
 console.log(msg);
