17ad403dd12036f99fdaf1ba344d93f48f1120db
Nildo <nildo@webstack.com.br>
2022-04-05 18:34:44 -0300

    finalizado exemplos par revisão de objects

--- objetosPrototypeAvancados/revisandoObjetos2.js
+++ objetosPrototypeAvancados/revisandoObjetos2.js
@@ -15,14 +15,35 @@
 
 
 //  constructor function
-function Pessoa (nome, sobrenome) { // parâmetros da função construtura
-    this.nome = nome;
-    this.sobrenome = sobrenome;
-}
+// function Pessoa (nome, sobrenome) { // parâmetros da função construtura
+//     this.nome = nome;
+//     this.sobrenome = sobrenome;
+// }
 
-const p1 = new Pessoa('Fulano', 'de Tal');
-const p2 = new Pessoa('Beltrano', 'de Tal');
+// const p1 = new Pessoa('Fulano', 'de Tal');
+// const p2 = new Pessoa('Beltrano', 'de Tal');
 
-console.log(p1);
-// console.log(typeof(p1));
-console.log(p2);
+// console.log(p1);
+// // console.log(typeof(p1));
+// console.log(p2);
+
+
+
+/*
+    Sintaxe
+    Object.defineProperty(obj, prop, descriptor)
+
+    Fonte MDN: url(https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty)
+*/
+
+
+// exemplo 2
+const object1 = {}
+
+Object.defineProperty(object1, 'property1', {
+    value: 42,
+    writable: false
+});
+object1.property1 = 77;
+
+console.log(object1.property1);
