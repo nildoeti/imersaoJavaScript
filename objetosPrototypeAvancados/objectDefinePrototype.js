0c904353f56b5193c2965e7eb12d01652981d034
Nildo <nildo@webstack.com.br>
2022-04-07 15:59:39 -0300

    adicionado exemplo 2 para o projeto

--- objetosPrototypeAvancados/objectDefinePrototype.js
+++ objetosPrototypeAvancados/objectDefinePrototype.js
@@ -6,13 +6,27 @@
 */
 
 // exemplo 1
+// var person = { // object person com a propriedade name e valor Leela
+// 	id: 1, // propriedade pública pois pode ser acessado de fora do objeto
+//	name: 'Leela'
+// }
+
+// console.log(person.id); // valor atual: 1
+// person.id = 4; // propriedade id por ser pública, pode também ser modificada
+// console.log(person.id); // novo valor: 4
+// person.id = 10;
+// console.log(person.id); // 10
+
+// exemplo 2
 var person = { // object person com a propriedade name e valor Leela
-	id: 1, // propriedade pública pois pode ser acessado de fora do objeto
 	name: 'Leela'
-}
+};
+console.log(person.id); // undefined, pois esta propriedade não existe
 
-console.log(person.id); // valor atual: 1
-person.id = 4; // propriedade id por ser pública, pode também ser modificada
-console.log(person.id); // novo valor: 4
+Object.defineProperty(person, 'id', { // propriedade protegida id
+	value: 1,
+});
+console.log(person.id); // 1
 
-
+person.id = 4; // tentando alterar o valor para a propriedade id
+console.log(person.id); // 1
