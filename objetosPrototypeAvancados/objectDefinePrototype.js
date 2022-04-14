ec3052a58847fe2b0f1f71b8fe51099af2b220af
Nildo <nildo@webstack.com.br>
2022-04-13 23:37:11 -0300

    adicioando exemplo para listar todas as propriedades de uma função construtora

--- objetosPrototypeAvancados/objectDefinePrototype.js
+++ objetosPrototypeAvancados/objectDefinePrototype.js
@@ -18,15 +18,41 @@
 // console.log(person.id); // 10
 
 // exemplo 2
-var person = { // object person com a propriedade name e valor Leela
-	name: 'Leela'
-};
-console.log(person.id); // undefined, pois esta propriedade não existe
+// var person = { // object person com a propriedade name e valor Leela
+// 	name: 'Leela'
+// };
+// console.log(person.id); // undefined, pois esta propriedade não existe
+//
+// Object.defineProperty(person, 'id', { // propriedade protegida id
+// 	value: 1,
+// });
+// console.log(person.id); // 1
+//
+// person.id = 4; // tentando alterar o valor para a propriedade id
+// console.log(person.id); // 1
 
-Object.defineProperty(person, 'id', { // propriedade protegida id
-	value: 1,
-});
-console.log(person.id); // 1
 
-person.id = 4; // tentando alterar o valor para a propriedade id
-console.log(person.id); // 1
+// exemplo 3
+// var person = { // object person com a propriedade name e valor Leela
+// 	name: 'Leela'
+// };
+//
+// Object.defineProperty(person, 'id', {
+// 	value: 1,
+// 	writable: true // permite que a propriedade id, possa ser alterada
+// });
+// console.log(person.id); // 1
+//
+// person.id = 10;
+// console.log(person.id); // 10
+
+
+// exemplo 4
+function Produto(nome, preco, estoque) {
+	this.nome = nome;
+	this.preco = preco;
+}
+
+const p1 = new Produto('Camiseta', 50, 3);
+
+console.log(p1); // Produto { nome: 'Camiseta', preco: 50 }
