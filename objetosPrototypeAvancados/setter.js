c08a67881c996b0038ae8322989c658557ae2304
Nildo <nildo@webstack.com.br>
2022-04-15 08:44:16 -0300

    adicionado exemplo para setter com getter

--- objetosPrototypeAvancados/setter.js
+++ objetosPrototypeAvancados/setter.js
@@ -1,1 +1,27 @@
-setter.js
+// setter -> protege uma propriedade
+
+function Produto(nome, preco, estoque) {
+    this.nome = nome;
+    this.preco = preco;
+
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true, // exibe a chave
+        configurable: true, // configurável
+
+        get: function(){
+            return estoque;
+        },
+
+        set: function(valor) {
+            console.log(valor);
+        }
+    });
+}
+
+const p1 = new Produto('Camiseta', 40, 3);
+
+console.log(p1); // Produto { nome: 'Camiseta', preco: 40, estoque: [Getter] }
+// console.log(p1.estoque); // 3
+
+p1.estoque = 'Qualquer valor';
+console.log(p1.estoque); // Qualquer valor, 3
