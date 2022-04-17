3ba71de45969c49c753312c61373be3970b1c3e4
Nildo <nildo@webstack.com.br>
2022-04-17 07:11:32 -0300

    adicionado exemplo 3 para get e set

--- objetosPrototypeAvancados/setter.js
+++ objetosPrototypeAvancados/setter.js
@@ -1,27 +1,57 @@
 // setter -> protege uma propriedade
 
-function Produto(nome, preco, estoque) {
-    this.nome = nome;
-    this.preco = preco;
+// function Produto(nome, preco, estoque) {
+//     this.nome = nome;
+//     this.preco = preco;
 
-    Object.defineProperty(this, 'estoque', {
-        enumerable: true, // exibe a chave
-        configurable: true, // configurável
+//     Object.defineProperty(this, 'estoque', {
+//         enumerable: true, // exibe a chave
+//         configurable: true, // configurável
 
-        get: function(){
-            return estoque;
+//         get: function(){
+//             return estoquePrivado;
+//         },
+
+//         // exemplo 2
+//         set: function(valor) {
+//             if (typeof valor !== 'number') {
+//                 throw new TypeError('Mensagem');
+//             }
+//             estoquePrivado = valor;
+//         }
+
+//         // exemplo 1
+//         // set: function(valor) {
+//         //     if (typeof valor != 'number') {
+//         //         console.log('Bad value');
+//         //         return;
+//         //     }
+//         //     estoquePrivado = valor;
+//         // }
+//     });
+// }
+
+// const p1 = new Produto('Camiseta', 40, 3);
+
+// console.log(p1); // Produto { nome: 'Camiseta', preco: 40, estoque: [Getter] }
+// // console.log(p1.estoque); // 3
+
+// p1.estoque = 500;
+// console.log(p1.estoque); // Qualquer valor, 3
+
+
+// exemplo 3
+function criaProduto(nome) {
+    return {
+        get nome() {
+            return nome
         },
-
-        set: function(valor) {
-            console.log(valor);
+        set nome(valor) {
+            return valor;
         }
-    });
+    };
 }
 
-const p1 = new Produto('Camiseta', 40, 3);
-
-console.log(p1); // Produto { nome: 'Camiseta', preco: 40, estoque: [Getter] }
-// console.log(p1.estoque); // 3
-
-p1.estoque = 'Qualquer valor';
-console.log(p1.estoque); // Qualquer valor, 3
+const p2 = criaProduto('Camiseta');
+console.log(p2); // { nome: [Getter/Setter] }
+console.log(p2.nome); // Camiseta
