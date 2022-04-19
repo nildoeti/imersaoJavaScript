a53c3d93d9eef74ae9e8d9ab167b991d5733e9d3
Nildo <nildo@webstack.com.br>
2022-04-19 10:12:57 -0300

    finalizado exemplo para a criação de métodos para a função construtora a partir do prototype

--- objetosPrototypeAvancados/manipulandoPrototype.js
+++ objetosPrototypeAvancados/manipulandoPrototype.js
@@ -18,7 +18,7 @@ Object.setPrototypeOf(objC, objB) // objC herda o proto de objB
 
 /*
     Saida para os exemplos acima, pelo console do navegador
-    
+
     objC
     Object { chaveC: "C" }
     ​
@@ -34,3 +34,32 @@ Object.setPrototypeOf(objC, objB) // objC herda o proto de objB
     ​​​
     <prototype>: Object { … }
 */
+
+
+
+// exemplo para protorype com uso de funções
+function Produto(nome, preco) {
+    this.nome = nome;
+    this.preco = preco
+}
+
+// método para desconto 
+// adicionar métodos sempre fora da função construtora
+Produto.prototype.desconto = function (percentual) {
+    this.preco = this.preco - (this.preco * (percentual / 100));
+};
+
+// método para aumento 
+// adicionar métodos sempre fora da função construtora
+Produto.prototype.aumento = function (percentual) {
+    this.preco = this.preco + (this.preco * (percentual / 100));
+};
+
+
+// criando o produto p1
+const p1 = new Produto('Camiseta', 50);
+// p1.desconto(100); // adicioando o desconto
+p1.aumento(100);
+console.log(p1); // Produto { nome: 'Camiseta', preco: 80 }
+
+
