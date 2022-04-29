73e831008000cad8505d21c09cc69105d5488364
Nildo <nildo@webstack.com.br>
2022-04-29 12:16:54 -0300

    adicionado  exemplo de uso de map no javascript

---
+++ array/array-2.js
@@ -0,0 +1,9 @@
+// exemplo de uso de map no javascript
+const numbers = [1, 2, 3]
+
+const myNumbers = numbers.map(item => item * 2)
+
+
+console.log(myNumbers);
+console.log(numbers === myNumbers);
+console.log(numbers, myNumbers);
