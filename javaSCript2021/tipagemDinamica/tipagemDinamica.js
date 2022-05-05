28796d4d9a461125c05a780e9010768164228a39
Nildo <nildo@webstack.com.br>
2022-05-05 07:34:39 -0300

    finalizado script para o projeto

---
+++ javaSCript2021/tipagemDinamica/tipagemDinamica.js
@@ -0,0 +1,17 @@
+/**
+ * Tipagem dinâmica no Java Script, efine ela como uma linguágem em que
+ * não precisa definir o tipo de dados das vaiáveis como acontence  com outras
+ * linguagens como C, C#, JAVA etc.
+ */
+
+let idade = 9; // assume o tipo Number
+console.log(typeof(idade)); // number
+
+let nome = 'Filomena'; // tipo string
+console.log(typeof(nome));
+
+let maiorQue = 18; // tipo boolean
+console.log(typeof(maiorQue === 18));
+
+let tipoNulo = null; // tipo nulo
+console.log(tipoNulo);
