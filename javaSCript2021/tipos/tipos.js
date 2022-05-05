1a13b51bbcb79c4448824ebee3a3102a046082c4
Nildo <nildo@webstack.com.br>
2022-05-04 23:07:26 -0300

    adicionado exemplos para tipos em javascript

---
+++ javaSCript2021/tipos/tipos.js
@@ -0,0 +1,7 @@
+//  tipos referência
+let nome = 'Fulano'; // string literal
+let idade = 9; // tipo numeral (Number)
+let maiorDeIdade = true; // tipo boolean
+let login = false; // tipo boolean
+let sobrenome; // tipo undefined
+let cor = null; // tip nulo - sem valor
