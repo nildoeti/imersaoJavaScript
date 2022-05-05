e06870913c44eaa4675319b6133127b95bc5cd3b
Nildo <nildo@webstack.com.br>
2022-05-05 08:08:49 -0300

    finalizado exemplos para array em javascript

---
+++ javaSCript2021/array/array.js
@@ -0,0 +1,16 @@
+// array são estruturas para armazenamento de tipos de dados iguais ou diferentes
+
+// array de numeros
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+// array de strings
+let cores = ['amarelo', 'verde', 'azul']
+
+// exibindo no console
+console.log(numeros); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+// exibindo pelo indíce 
+console.log(numeros[0]); // 1
+
+// verificando a quantidade de items no array
+console.log(numeros.length);
