0ab4ea075afb3262fafd224eba2f3275bcb44d05
Nildo <nildo@webstack.com.br>
2022-05-04 21:32:11 -0300

    finalizado script  do projeto

---
+++ javaSCript2021/variaveis/variaveis.js
@@ -0,0 +1,12 @@
+let idade = 5;
+let nome = 'Susane'
+const MAIOR = 1000;
+let arrayVazio = [] // arrays em JavaScript são considerados objetos
+let objectVazio = {}
+
+// verificando o tipo de dados armazenados nas variáveis
+console.log(typeof(idade));
+console.log(typeof(nome));
+console.log(typeof(MAIOR));
+console.log(typeof(arrayVazio));
+console.log(typeof(objectVazio));
