47e59924c45c5039b30115752bd264a722026556
Nildo <nildo@webstack.com.br>
2022-06-20 13:47:13 -0300

    adiciondo primeira e segunda forma de acessar a propriedade de um objeto

---
+++ objetosPrototypeAvancados/object-1.js
@@ -0,0 +1,15 @@
+// exemplo 1 de objeto
+const pessoa = {
+    nome: 'Fulano de',
+    sobreNome: 'Tal'
+}
+
+// primeira forma de acessar as propriedades do objeto
+console.log(pessoa.nome);
+console.log(pessoa.sobreNome);
+// segunda forma de acessar as propriedades do objeto
+console.log(pessoa['nome']);
+console.log(pessoa['sobreNome']);
+
+
+
