d4b16f9f3f02a23c5c81c0f927590a6c7aa4c281
Nildo <nildo@webstack.com.br>
2022-06-21 10:09:10 -0300

    adicionado exemplos para factory function ao projeto

---
+++ objetosPrototypeAvancados/moldesDeFuncoesParaCriarNovosObjetos.js
@@ -0,0 +1,30 @@
+// moldes de funções para criar objetos
+// factory function -> funções de fábrica
+function criaPessoa (nome, sobrenome) {
+    return {
+        nome,
+        sobrenome,
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`;
+        }
+    };
+}
+
+const p1 = criaPessoa('Beltrano de', 'Tal');
+console.log(p1.nomeCompleto);
+
+function criaProduto(nome, codigo) {
+    return {
+        nome,
+        codigo,
+        get produto() {
+            return `${this.nome} código: ${this.codigo}`;
+        }
+    };
+}
+
+const pd1 = criaProduto('Sabão', 4562);
+console.log(pd1.produto);
+console.log(typeof(pd1));
+console.log(pd1.nome);
+console.log(pd1.codigo);
